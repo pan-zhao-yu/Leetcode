6ba20bc418cb0d2163cc0f59d611c7088c256533
Joey <59043772+pan-zhao-yu@users.noreply.github.com>
2024-01-11 16:26:40 +1100

    Time: 3 ms (100.00%), Space: 68.6 MB (17.09%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -10,31 +10,60 @@
  */
 class Solution {
     public boolean isPalindrome(ListNode head) {
-        if(head == null || head.next == null){
-            return true;
-        }
-        ListNode left = head, right = head;
+        if(head == null) return true;
+        ListNode mid = head;
+        ListNode right = head;
+        
         while(right != null && right.next != null){
-            left = left.next;
+            mid = mid.next;
             right = right.next.next;
         }
         
-        ListNode pre = null, next;
-        while(left != null){
-            next = left.next;
-            left.next = pre;
-            pre = left;
-            left = next;
+        ListNode prev = null;
+        ListNode next = null;
+        while(mid != null){
+            next = mid.next;
+            mid.next = prev;
+            prev = mid;
+            mid = next;
         }
         
-        while(pre != null && head != null){
-            if(pre.val != head.val){
-                return false;
-            }
-            pre = pre.next;
+        while(prev != null && head != null){
+            if(prev.val != head.val) return false;
+            prev = prev.next;
             head = head.next;
         }
         return true;
+        
+        
+        
+        
+        
+//         if(head == null || head.next == null){
+//             return true;
+//         }
+//         ListNode left = head, right = head;
+//         while(right != null && right.next != null){
+//             left = left.next;
+//             right = right.next.next;
+//         }
+        
+//         ListNode pre = null, next;
+//         while(left != null){
+//             next = left.next;
+//             left.next = pre;
+//             pre = left;
+//             left = next;
+//         }
+        
+//         while(pre != null && head != null){
+//             if(pre.val != head.val){
+//                 return false;
+//             }
+//             pre = pre.next;
+//             head = head.next;
+//         }
+//         return true;
         
     }
 }
