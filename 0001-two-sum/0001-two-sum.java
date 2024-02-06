3c947e506337e564d825ee0166784060254e3d00
Joey <59043772+pan-zhao-yu@users.noreply.github.com>
2024-02-06 14:34:56 +1100

    Time: 2 ms (94.86%), Space: 44.9 MB (40.99%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,14 +1,47 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int n = nums.length;
-        for(int i = 0; i <= n - 1; i++){
-            for(int j = i + 1; j <= n - 1; j++){
-                if(nums[i] + nums[j] == target){
-                    return new int[] {i, j};
-                }
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < nums.length; i++){
+            int complement = target - nums[i];
+            if(map.containsKey(complement)){
+                return new int[]{i, map.get(complement)};
             }
+            map.put(nums[i], i);
         }
         return new int[]{};
     }
 }
+
+
+
+// class Solution {
+//     public int[] twoSum(int[] nums, int target) {
+//         Map<Integer, Integer> map = new HashMap<>();
+//         int n = nums.length;
+//         for(int i = 0; i < n; i++){
+//             map.put(nums[i], i);
+//         }
+//         for(int i = 0; i < n; i++){
+//             int complement = target - nums[i];
+//             if(map.containsKey(complement) && map.get(complement) != i){
+//                 return new int[]{i, map.get(complement)};
+//             }
+//         }
+//         return new int[]{};
+//     }
+// }
+
+// class Solution {
+//     public int[] twoSum(int[] nums, int target) {
+//         int n = nums.length;
+//         for(int i = 0; i <= n - 1; i++){
+//             for(int j = i + 1; j <= n - 1; j++){
+//                 if(nums[i] + nums[j] == target){
+//                     return new int[] {i, j};
+//                 }
+//             }
+//         }
+//         return new int[]{};
+//     }
+// }
 
