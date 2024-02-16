e07811f842c2009ad4f5e8374660d357e753f149
Joey <59043772+pan-zhao-yu@users.noreply.github.com>
2024-02-16 13:50:55 +1100

    Time: 10 ms (88.75%), Space: 60.6 MB (24.17%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -8,14 +8,3 @@ class Solution {
         return false;
     }
 }
-
-
-//         HashSet <Integer> seen = new HashSet<>();
-//         for (int i =0; i < nums.length; i++){
-//             if (seen.contains(nums[i])){
-//                 return true;
-//             }else{
-//                 seen.add(nums[i]);
-//             }
-//         }
-//         return false;
