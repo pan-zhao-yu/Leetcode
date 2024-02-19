cd6bd0f3b8c871c8b7fb8760eff79bc0fd66b2dc
Joey <59043772+pan-zhao-yu@users.noreply.github.com>
2024-02-19 16:39:20 +1100

    Time: 16 ms (36.63%), Space: 45.2 MB (97.19%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,20 +1,43 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
         Map<Integer, Integer> map = new HashMap<>();
-        for (int num : nums) {
-            map.put(num, map.getOrDefault(num, 0) + 1);
+        for(int i = 0; i < nums.length; i++){
+            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
         }
-
+        
         PriorityQueue<Integer> heap = new PriorityQueue<>((n1, n2) -> map.get(n1) - map.get(n2));
-        for (int num : map.keySet()) {
+        for(int num : map.keySet()){
             heap.add(num);
-            if (heap.size() > k) heap.poll();
+            if(heap.size() > k) heap.poll();
         }
-
-        int[] topK = new int[k];
-        for (int i = k - 1; i >= 0; --i) {
-            topK[i] = heap.poll();
+        
+        int[] res = new int[k];
+        for(int i = k - 1; i >= 0; --i){
+            res[i] = heap.poll();
         }
-        return topK;
+        return res;
     }
 }
+
+
+
+// class Solution {
+//     public int[] topKFrequent(int[] nums, int k) {
+//         Map<Integer, Integer> map = new HashMap<>();
+//         for (int num : nums) {
+//             map.put(num, map.getOrDefault(num, 0) + 1);
+//         }
+
+//         PriorityQueue<Integer> heap = new PriorityQueue<>((n1, n2) -> map.get(n1) - map.get(n2));
+//         for (int num : map.keySet()) {
+//             heap.add(num);
+//             if (heap.size() > k) heap.poll();
+//         }
+
+//         int[] topK = new int[k];
+//         for (int i = k - 1; i >= 0; --i) {
+//             topK[i] = heap.poll();
+//         }
+//         return topK;
+//     }
+// }
