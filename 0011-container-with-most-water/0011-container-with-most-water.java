cac441b8adb784fc0459d675d512269984d12386
Joey <59043772+pan-zhao-yu@users.noreply.github.com>
2024-03-15 14:38:00 +1100

    Time: 5 ms (71.30%), Space: 57.2 MB (85.95%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -1,19 +1,39 @@
 class Solution {
     public int maxArea(int[] height) {
         int left = 0;
-        int right = height.length -1;
-        int maxArea = 0;
+        int right = height.length - 1;
+        int maxAmount = 0;
         
         while(left < right){
             int width = right - left;
             int minHeight = Math.min(height[left], height[right]);
-            maxArea = Math.max(maxArea, width * minHeight);
+            maxAmount = Math.max(maxAmount, width * minHeight);
             if(height[left] < height[right]){
                 left++;
             }else{
                 right--;
             }
         }
-        return maxArea;
+        return maxAmount;
     }
 }
+
+// class Solution {
+//     public int maxArea(int[] height) {
+//         int left = 0;
+//         int right = height.length -1;
+//         int maxArea = 0;
+        
+//         while(left < right){
+//             int width = right - left;
+//             int minHeight = Math.min(height[left], height[right]);
+//             maxArea = Math.max(maxArea, width * minHeight);
+//             if(height[left] < height[right]){
+//                 left++;
+//             }else{
+//                 right--;
+//             }
+//         }
+//         return maxArea;
+//     }
+// }
