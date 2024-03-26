7ca7a7ee0de1b7e06c8e50aff137b5ed8ba3d483
Joey <59043772+pan-zhao-yu@users.noreply.github.com>
2024-03-26 16:19:38 +1100

    Time: 1 ms (88.17%), Space: 44.9 MB (53.38%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
@@ -16,19 +16,18 @@
 class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
         List<List<Integer>> result = new ArrayList<>();
-        Queue <TreeNode> queue = new LinkedList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
         
         if(root == null){
             return result;
         }
-        
         queue.offer(root);
         while(!queue.isEmpty()){
             int size = queue.size();
-            List<Integer> currentLevel = new ArrayList<>();
+            List<Integer> currLevel = new ArrayList<>();
             for(int i = 0; i < size; i++){
                 TreeNode temp = queue.poll();
-                currentLevel.add(temp.val);
+                currLevel.add(temp.val);
                 if(temp.left != null){
                     queue.offer(temp.left);
                 }
@@ -36,8 +35,37 @@ class Solution {
                     queue.offer(temp.right);
                 }
             }
-            result.add(currentLevel);
+            result.add(currLevel);
         }
         return result;
     }
 }
+
+// class Solution {
+//     public List<List<Integer>> levelOrder(TreeNode root) {
+//         List<List<Integer>> result = new ArrayList<>();
+//         Queue <TreeNode> queue = new LinkedList<>();
+        
+//         if(root == null){
+//             return result;
+//         }
+        
+//         queue.offer(root);
+//         while(!queue.isEmpty()){
+//             int size = queue.size();
+//             List<Integer> currentLevel = new ArrayList<>();
+//             for(int i = 0; i < size; i++){
+//                 TreeNode temp = queue.poll();
+//                 currentLevel.add(temp.val);
+//                 if(temp.left != null){
+//                     queue.offer(temp.left);
+//                 }
+//                 if(temp.right != null){
+//                     queue.offer(temp.right);
+//                 }
+//             }
+//             result.add(currentLevel);
+//         }
+//         return result;
+//     }
+// }
