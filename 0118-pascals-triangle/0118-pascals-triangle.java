d4b2dc11f7c51b687a5e08d5e0c8497e9ce74a91
Joey <59043772+pan-zhao-yu@users.noreply.github.com>
2024-03-29 23:35:00 +1100

    Time: 1 ms (85.81%), Space: 41.8 MB (72.39%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.java
+++ 0118-pascals-triangle/0118-pascals-triangle.java
@@ -1,18 +1,41 @@
+// class Solution {
+//     public List<List<Integer>> generate(int numRows) {
+//         List<List<Integer>> result = new ArrayList<>();
+//         if(numRows <= 0) return result;
+//         result.add(Arrays.asList(1));
+//         for(int i = 1; i < numRows; i++){
+//             List<Integer> row = new ArrayList<>();
+//             List<Integer> prevRow = result.get(i - 1);
+//             row.add(1);
+//             for(int j = 1; j < i; j++){
+//                 row.add(prevRow.get(j - 1) + prevRow.get(j));
+//             }
+//             row.add(1);
+//             result.add(row);
+//         }
+//         return result;
+//     }
+// }
+
 class Solution {
     public List<List<Integer>> generate(int numRows) {
-        List<List<Integer>> result = new ArrayList<>();
-        if(numRows <= 0) return result;
-        result.add(Arrays.asList(1));
-        for(int i = 1; i < numRows; i++){
-            List<Integer> row = new ArrayList<>();
-            List<Integer> prevRow = result.get(i - 1);
-            row.add(1);
-            for(int j = 1; j < i; j++){
-                row.add(prevRow.get(j - 1) + prevRow.get(j));
+        
+        List<List<Integer>> res = new ArrayList<>();
+        
+        for(int i=0; i<numRows; i++){
+            List<Integer> list = new ArrayList<>();
+            
+            for(int j=0; j<=i; j++){
+                if(j>0 && i>0 && res.get(i-1).size()-1>=j){
+                    list.add(res.get(i-1).get(j-1)+res.get(i-1).get(j));
+                } else{
+                    list.add(1);
+                }
             }
-            row.add(1);
-            result.add(row);
+            res.add(list);
         }
-        return result;
+        
+        return res;
+        
     }
 }
