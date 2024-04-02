c542f7c9db830166771075e552ee6cb75600c663
Joey <59043772+pan-zhao-yu@users.noreply.github.com>
2024-04-02 23:54:55 +1100

    Time: 17 ms (41.42%), Space: 44.7 MB (62.90%) - LeetHub

--- 0929-unique-email-addresses/0929-unique-email-addresses.java
+++ 0929-unique-email-addresses/0929-unique-email-addresses.java
@@ -1,19 +1,32 @@
 class Solution {
     public int numUniqueEmails(String[] emails) {
-        HashSet<String> uniqueEmails = new HashSet<>();
-        for(String email : emails){
-            String formatEmail = getFormatEmail(email);
-            uniqueEmails.add(formatEmail);
+        Set<String> normalized = new HashSet<>(); // used to save simplified email address, cost O(n) sapce.
+        for (String email : emails) {
+            String[] parts = email.split("@"); // split into local and domain parts.
+            String[] local = parts[0].split("\\+"); // split local by '+'.
+            normalized.add(local[0].replace(".", "") + "@" + parts[1]); // remove all '.', and concatenate '@' and domain.        
         }
-        return uniqueEmails.size();
-    }
-    private String getFormatEmail(String email){
-        String[] separet = email.split("@");
-        String local = separet[0];
-        String domain = separet[1];
-        local = local.replace(".", "");
-        String[] localPlus = local.split("\\+");
-        local = localPlus[0];
-        return local + "@" + domain;
+        return normalized.size();
     }
 }
+
+
+// class Solution {
+//     public int numUniqueEmails(String[] emails) {
+//         HashSet<String> uniqueEmails = new HashSet<>();
+//         for(String email : emails){
+//             String formatEmail = getFormatEmail(email);
+//             uniqueEmails.add(formatEmail);
+//         }
+//         return uniqueEmails.size();
+//     }
+//     private String getFormatEmail(String email){
+//         String[] separet = email.split("@");
+//         String local = separet[0];
+//         String domain = separet[1];
+//         local = local.replace(".", "");
+//         String[] localPlus = local.split("\\+");
+//         local = localPlus[0];
+//         return local + "@" + domain;
+//     }
+// }
