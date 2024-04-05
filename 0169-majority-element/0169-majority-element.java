0b6beb80f35ae5b5b04b3e5d79626c1fe1faa0a5
Joey <59043772+pan-zhao-yu@users.noreply.github.com>
2024-04-05 20:25:22 +1100

    Time: 15 ms (20.41%), Space: 48.9 MB (88.03%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,34 +1,38 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        int result = nums[0];
-        int count = 1;
-        
-        for(int i = 1; i < nums.length; i++){
-            if(count == 0){
-                result = nums[i];
-                count ++;
-            }else if(result == nums[i]){
-                count ++;
+        Map<Integer, Integer> map = new HashMap<>();
+        int n = nums.length / 2;
+        for(int i = 0; i < nums.length; i++){
+            if(map.containsKey(nums[i])){
+                map.put(nums[i], map.get(nums[i]) + 1);
             }else{
-                count --;
+                map.put(nums[i], 1);
             }
         }
-        return result;
         
-        
-        
-//         int n = nums.length / 2;
-//         Map<Integer, Integer> map = new HashMap<>();
-//         for(int i = 0; i < nums.length; i++){
-//             map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
-//         }
-        
-//         for(int i = 0; i < nums.length; i++){
-//             if(map.get(nums[i]) > n){
-//                 return nums[i];
-//             }
-            
-//         }
-//         return 0;
+        for(Integer num : map.keySet()){
+            if(map.get(num) > n) return num;
+        }
+        return 0;
     }
 }
+
+
+// class Solution {
+//     public int majorityElement(int[] nums) {
+//         int result = nums[0];
+//         int count = 1;
+        
+//         for(int i = 1; i < nums.length; i++){
+//             if(count == 0){
+//                 result = nums[i];
+//                 count ++;
+//             }else if(result == nums[i]){
+//                 count ++;
+//             }else{
+//                 count --;
+//             }
+//         }
+//         return result;
+//     }
+// }
