f76c10d685eb0ff639d79ad9cfb1ad1a8ac0097b
Joey <59043772+pan-zhao-yu@users.noreply.github.com>
2024-04-06 01:46:48 +1100

    Time: 68 ms (24.54%), Space: 54.8 MB (99.34%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.java
+++ 0912-sort-an-array/0912-sort-an-array.java
@@ -3,38 +3,77 @@ class Solution {
         mergeSort(nums, 0, nums.length - 1);
         return nums;
     }
-    
-    private void mergeSort(int[] nums, int low, int high){
+    private void mergeSort(int[] arr, int low, int high){
         if(low >= high) return;
         int mid = low + (high - low) / 2;
-        mergeSort(nums, low, mid);
-        mergeSort(nums, mid + 1, high);
-        merge(nums, low, mid, high);
+        mergeSort(arr, low, mid);
+        mergeSort(arr, mid + 1, high);
+        merge(arr, low, mid, high);
     }
-    
-    private void merge(int[] nums, int low, int mid, int high){
-        ArrayList<Integer> arr = new ArrayList<>();
+    private void merge(int[] arr, int low, int mid, int high){
         int left = low;
         int right = mid + 1;
+        ArrayList<Integer> temp = new ArrayList<>();
         while(left <= mid && right <= high){
-            if(nums[left] < nums[right]){
-                arr.add(nums[left]);
+            if(arr[left] < arr[right]){
+                temp.add(arr[left]);
                 left++;
             }else{
-                arr.add(nums[right]);
+                temp.add(arr[right]);
                 right++;
             }
         }
         while(left <= mid){
-            arr.add(nums[left]);
+            temp.add(arr[left]);
             left++;
         }
         while(right <= high){
-            arr.add(nums[right]);
+            temp.add(arr[right]);
             right++;
         }
         for(int i = low; i <= high; i++){
-            nums[i] = arr.get(i - low);
+            arr[i] = temp.get(i - low);
         }
     }
 }
+
+// class Solution {
+//     public int[] sortArray(int[] nums) {
+//         mergeSort(nums, 0, nums.length - 1);
+//         return nums;
+//     }
+    
+//     private void mergeSort(int[] nums, int low, int high){
+//         if(low >= high) return;
+//         int mid = low + (high - low) / 2;
+//         mergeSort(nums, low, mid);
+//         mergeSort(nums, mid + 1, high);
+//         merge(nums, low, mid, high);
+//     }
+    
+//     private void merge(int[] nums, int low, int mid, int high){
+//         ArrayList<Integer> arr = new ArrayList<>();
+//         int left = low;
+//         int right = mid + 1;
+//         while(left <= mid && right <= high){
+//             if(nums[left] < nums[right]){
+//                 arr.add(nums[left]);
+//                 left++;
+//             }else{
+//                 arr.add(nums[right]);
+//                 right++;
+//             }
+//         }
+//         while(left <= mid){
+//             arr.add(nums[left]);
+//             left++;
+//         }
+//         while(right <= high){
+//             arr.add(nums[right]);
+//             right++;
+//         }
+//         for(int i = low; i <= high; i++){
+//             nums[i] = arr.get(i - low);
+//         }
+//     }
+// }
