7717cdef2b9c4c45fa79f59b183fe7b2f6d2c2e3
Joey <59043772+pan-zhao-yu@users.noreply.github.com>
2024-04-12 18:30:00 +1000

    Time: 2 ms (63.15%), Space: 42.4 MB (87.09%) - LeetHub

--- 0535-encode-and-decode-tinyurl/0535-encode-and-decode-tinyurl.java
+++ 0535-encode-and-decode-tinyurl/0535-encode-and-decode-tinyurl.java
@@ -1,18 +1,37 @@
 public class Codec {
-    public Map<String, String> map = new HashMap<>();
+    public HashMap<String, String> urlMap = new HashMap<>();
+    String baseUrl = "https://tinyurl.com/";
+    int counter = 0;
     // Encodes a URL to a shortened URL.
     public String encode(String longUrl) {
-        String key = "keeyy";
-        map.put(key, longUrl);
-        return key;
+        String key = String.valueOf(counter++);
+        String shortUrl = baseUrl + key;
+        urlMap.put(shortUrl, longUrl);
+        return shortUrl;
     }
 
     // Decodes a shortened URL to its original URL.
     public String decode(String shortUrl) {
-        return map.get(shortUrl);
+        return urlMap.get(shortUrl);
     }
 }
 
 // Your Codec object will be instantiated and called as such:
 // Codec codec = new Codec();
 // codec.decode(codec.encode(url));
+
+
+// public class Codec {
+//     public Map<String, String> map = new HashMap<>();
+//     // Encodes a URL to a shortened URL.
+//     public String encode(String longUrl) {
+//         String key = "keeyy";
+//         map.put(key, longUrl);
+//         return key;
+//     }
+
+//     // Decodes a shortened URL to its original URL.
+//     public String decode(String shortUrl) {
+//         return map.get(shortUrl);
+//     }
+// }
