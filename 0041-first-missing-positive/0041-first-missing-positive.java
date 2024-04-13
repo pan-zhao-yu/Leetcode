c7b2360ca64b3c8bbfa9713e0a008b30dc4a5151
Joey <59043772+pan-zhao-yu@users.noreply.github.com>
2024-04-14 00:03:28 +1000

    Time: 1 ms (96.02%), Space: 57 MB (22.53%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.java
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -1,40 +1,17 @@
 class Solution {
     public int firstMissingPositive(int[] nums) {
-        for(int i = 0; i < nums.length; i++){
-            while(nums[i] > 0 && nums[i] <= nums.length && nums[nums[i] - 1] != nums[i]){
+        int n = nums.length;
+        for(int i = 0; i < n; i++){
+            while(nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] != nums[i]){
                 int temp = nums[nums[i] - 1];
                 nums[nums[i] - 1] = nums[i];
                 nums[i] = temp;
             }
         }
         
-        for(int i = 0; i < nums.length; i++){
+        for(int i = 0; i < n; i++){
             if(nums[i] != i + 1) return i + 1;
         }
-        return nums.length + 1;
+        return n +1;
     }
 }
-
-// class Solution {
-//     public int firstMissingPositive(int[] nums) {
-//         int n = nums.length;
-
-//     // Move positive integers to their correct positions
-//     for (int i = 0; i < n; i++) {
-//         while (nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] != nums[i]) {
-//             int temp = nums[nums[i] - 1];
-//             nums[nums[i] - 1] = nums[i];
-//             nums[i] = temp;
-//         }
-//     }
-
-//     // Find the first missing positive integer
-//     for (int i = 0; i < n; i++) {
-//         if (nums[i] != i + 1) {
-//             return i + 1;
-//         }
-//     }
-
-//     return n + 1;
-//     }
-// }
