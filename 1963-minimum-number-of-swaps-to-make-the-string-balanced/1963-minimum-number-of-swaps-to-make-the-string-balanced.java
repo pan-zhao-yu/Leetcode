4dec9266f1ba1425b95a3db3d18016d0562f382c
Joey <59043772+pan-zhao-yu@users.noreply.github.com>
2024-04-14 02:13:37 +1000

    Time: 28 ms (30.86%), Space: 54.1 MB (66.96%) - LeetHub

--- 1963-minimum-number-of-swaps-to-make-the-string-balanced/1963-minimum-number-of-swaps-to-make-the-string-balanced.java
+++ 1963-minimum-number-of-swaps-to-make-the-string-balanced/1963-minimum-number-of-swaps-to-make-the-string-balanced.java
@@ -1,14 +1,15 @@
 class Solution {
     public int minSwaps(String s) {
-        if(s.length() == 0) return 0;
         int max = 0;
-        int extraclosing = 0;
+        int extraClosing = 0;
         for(int i = 0; i < s.length(); i++){
-            if(s.charAt(i)== ']'){
-                extraclosing++;
-                max = Math.max(extraclosing, max);
-            }else{
-                extraclosing--;
+            if(s.charAt(i) == ']' || s.charAt(i) == '['){
+                if(s.charAt(i) == ']'){
+                    extraClosing++;
+                    max = Math.max(extraClosing, max);
+                }else{
+                    extraClosing--;
+                }
             }
         }
         return (max + 1) / 2;
