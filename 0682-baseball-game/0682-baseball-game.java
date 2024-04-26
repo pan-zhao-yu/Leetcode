e6f2b79fa95ccca5cd052f7d4728f851a7d21f58
Joey <59043772+pan-zhao-yu@users.noreply.github.com>
2024-04-26 17:51:55 +1000

    Time: 2 ms (85.36%), Space: 41.4 MB (85.13%) - LeetHub

--- 0682-baseball-game/0682-baseball-game.java
+++ 0682-baseball-game/0682-baseball-game.java
@@ -1,27 +1,24 @@
 class Solution {
     public int calPoints(String[] operations) {
-        Stack<Integer> stack = new Stack<>();
-        if (operations.length == 0) return 0;
-        for(String op : operations){
-            if(op.equals("+") && stack.size() >= 2){
-                int score1 = stack.pop();
-                int score2 = stack.peek();
-                stack.push(score1);
-                stack.push(score1 + score2);
-            }else if(op.equals("D") && !stack.isEmpty()){
-                int score = stack.peek();
-                stack.push(score * 2);
-            }else if(op.equals("C") && !stack.isEmpty()){
-                stack.pop();
+        Stack<Integer> scores = new Stack<>();
+        int result = 0;
+        for(String operation : operations){
+            if(operation.equals("+") && scores.size() >= 2){
+                int s1 = scores.pop();
+                int s2 = scores.peek();
+                scores.push(s1);
+                scores.push(s2 + s1);
+            }else if(operation.equals("D") && !scores.isEmpty()){
+                scores.push(scores.peek() * 2);
+            }else if(operation.equals("C") && !scores.isEmpty()){
+                scores.pop();
             }else{
-                stack.push(Integer.parseInt(op));
+                scores.push(Integer.parseInt(operation));
             }
         }
-        int sum = 0;
-        while(!stack.isEmpty()){
-            sum += stack.pop();
+        while(!scores.isEmpty()){
+            result += scores.pop();
         }
-        return sum;
+        return result;
     }
 }
-
