9790b7152695477ee89b13891f7bb57d60aada31
Joey <59043772+pan-zhao-yu@users.noreply.github.com>
2024-05-13 03:56:24 +1000

    Time: 1 ms (99.79%), Space: 43.3 MB (56.51%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.java
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -1,18 +1,38 @@
 class Solution {
     public List<List<Integer>> subsetsWithDup(int[] nums) {
-        List<List<Integer>> list = new ArrayList<>();
+        List<List<Integer>> result = new ArrayList<>();
         Arrays.sort(nums);
-        backtrack(list, new ArrayList<>(), nums, 0);
-        return list;
+        backtrack(result, new ArrayList<>(), nums, 0);
+        return result;
     }
     
-    private void backtrack(List<List<Integer>> list, List<Integer> temp, int[] nums, int start){
-        list.add(new ArrayList<>(temp));
-        for(int i = start; i < nums.length; i ++){
-            if(i > start && nums[i] == nums[i -1]) continue;
+    private void backtrack(List<List<Integer>> result, List<Integer> temp, int[] nums, int start){
+        result.add(new ArrayList<>(temp));
+        for(int i = start; i < nums.length; i++){
+            if(i > start && nums[i] == nums[i - 1]) continue;
             temp.add(nums[i]);
-            backtrack(list, temp, nums, i+1);
-            temp.remove(temp.size() -1);
+            backtrack(result, temp, nums, i + 1);
+            temp.remove(temp.size() - 1);
         }
     }
 }
+
+
+// class Solution {
+//     public List<List<Integer>> subsetsWithDup(int[] nums) {
+//         List<List<Integer>> list = new ArrayList<>();
+//         Arrays.sort(nums);
+//         backtrack(list, new ArrayList<>(), nums, 0);
+//         return list;
+//     }
+    
+//     private void backtrack(List<List<Integer>> list, List<Integer> temp, int[] nums, int start){
+//         list.add(new ArrayList<>(temp));
+//         for(int i = start; i < nums.length; i ++){
+//             if(i > start && nums[i] == nums[i -1]) continue;
+//             temp.add(nums[i]);
+//             backtrack(list, temp, nums, i+1);
+//             temp.remove(temp.size() -1);
+//         }
+//     }
+// }
