85e33b4d258279358f25fec29c75c614a619fb59
Joey <59043772+pan-zhao-yu@users.noreply.github.com>
2024-05-13 12:52:01 +1000

    Time: 2 ms (47.66%), Space: 44.5 MB (27.10%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,20 +1,44 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
-        List<List<Integer>> list = new ArrayList<>();
-        backtrack(list, new ArrayList<>(), nums);
-        return list;
+        List<List<Integer>> result = new ArrayList<>();
+        backtrack(result, new ArrayList<>(), nums);
+        return result;
     }
     
-    private void backtrack(List<List<Integer>> list, List<Integer> temp, int[] nums){
+    private void backtrack(List<List<Integer>> result, List<Integer> temp, int[] nums){
         if(temp.size() == nums.length){
-            list.add(new ArrayList(temp));
+            result.add(new ArrayList(temp));
         }else{
             for(int i = 0; i < nums.length; i++){
                 if(temp.contains(nums[i])) continue;
                 temp.add(nums[i]);
-                backtrack(list, temp, nums);
+                backtrack(result, temp, nums);
                 temp.remove(temp.size() - 1);
             }
         }
+        
     }
 }
+
+
+
+// class Solution {
+//     public List<List<Integer>> permute(int[] nums) {
+//         List<List<Integer>> list = new ArrayList<>();
+//         backtrack(list, new ArrayList<>(), nums);
+//         return list;
+//     }
+    
+//     private void backtrack(List<List<Integer>> list, List<Integer> temp, int[] nums){
+//         if(temp.size() == nums.length){
+//             list.add(new ArrayList(temp));
+//         }else{
+//             for(int i = 0; i < nums.length; i++){
+//                 if(temp.contains(nums[i])) continue;
+//                 temp.add(nums[i]);
+//                 backtrack(list, temp, nums);
+//                 temp.remove(temp.size() - 1);
+//             }
+//         }
+//     }
+// }
