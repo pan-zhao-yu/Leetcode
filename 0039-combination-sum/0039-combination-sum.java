f2048f1d1b3930a6c524c774181c4668cc53eacf
Joey <59043772+pan-zhao-yu@users.noreply.github.com>
2024-05-15 14:28:34 +1000

    Time: 2 ms (84.83%), Space: 44.5 MB (59.82%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,20 +1,44 @@
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<Integer>> res = new ArrayList<>();
-        backtrack(res, new ArrayList<>(), candidates, target, 0);
-        return res;
+        List<List<Integer>> result = new ArrayList<>();
+        //Arrays.sort(candidates);
+        backtrack(result, new ArrayList<>(), candidates, target, 0);
+        return result;
     }
     
-    private void backtrack(List<List<Integer>> res, List<Integer> temp, int[] candidates, int remain, int start){
-        if(remain < 0) return;
-        else if(remain == 0){
-            res.add(new ArrayList(temp));
+    private void backtrack(List<List<Integer>> result, List<Integer> temp, int[] candidates, int remain, int start){
+        if(remain < 0){
+            return;
+        }else if(remain == 0){
+            result.add(new ArrayList(temp));
         }else{
             for(int i = start; i < candidates.length; i++){
                 temp.add(candidates[i]);
-                backtrack(res, temp, candidates, remain - candidates[i], i);
-                temp.remove(temp.size() -1);
+                backtrack(result, temp, candidates, remain - candidates[i], i);
+                temp.remove(temp.size() - 1);
             }
         }
     }
 }
+
+
+// class Solution {
+//     public List<List<Integer>> combinationSum(int[] candidates, int target) {
+//         List<List<Integer>> res = new ArrayList<>();
+//         backtrack(res, new ArrayList<>(), candidates, target, 0);
+//         return res;
+//     }
+    
+//     private void backtrack(List<List<Integer>> res, List<Integer> temp, int[] candidates, int remain, int start){
+//         if(remain < 0) return;
+//         else if(remain == 0){
+//             res.add(new ArrayList(temp));
+//         }else{
+//             for(int i = start; i < candidates.length; i++){
+//                 temp.add(candidates[i]);
+//                 backtrack(res, temp, candidates, remain - candidates[i], i);
+//                 temp.remove(temp.size() -1);
+//             }
+//         }
+//     }
+// }
