404037a66d6fdb2f3e22d9908e544b5d9a21d050
Joey <59043772+pan-zhao-yu@users.noreply.github.com>
2024-05-18 16:36:44 +1000

    Time: 0 ms (100.00%), Space: 41.9 MB (62.99%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -0,0 +1,33 @@
+class Solution {
+    private static final String[] KEYPAD = {
+        "",
+        "",
+        "abc",
+        "def",
+        "ghi",
+        "jkl",
+        "mno",
+        "pqrs",
+        "tuv",
+        "wxyz"
+    };
+    public List<String> letterCombinations(String digits) {
+        List<String> result = new ArrayList<>();
+        if(digits == null || digits.length() == 0) return result;
+        backtrack(result, new StringBuilder(), digits, 0);
+        return result;
+    }
+    
+    private void backtrack(List<String> result, StringBuilder temp, String digits, int start){
+        if(start == digits.length()) {
+            result.add(temp.toString());
+            return;
+        }
+        String letters = KEYPAD[digits.charAt(start) - '0'];
+        for(char letter : letters.toCharArray()){
+            temp.append(letter);
+            backtrack(result, temp, digits, start + 1);
+            temp.deleteCharAt(temp.length() - 1);
+        }
+    }
+}
