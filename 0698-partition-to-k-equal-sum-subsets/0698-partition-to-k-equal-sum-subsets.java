ef99b257acfeb4918532c9e8ea11b419f87ad908
Joey <59043772+pan-zhao-yu@users.noreply.github.com>
2024-05-25 01:06:47 +1000

    Time: 6 ms (76.67%), Space: 40.8 MB (92.00%) - LeetHub

--- 0698-partition-to-k-equal-sum-subsets/0698-partition-to-k-equal-sum-subsets.java
+++ 0698-partition-to-k-equal-sum-subsets/0698-partition-to-k-equal-sum-subsets.java
@@ -3,6 +3,8 @@ class Solution {
     public boolean canPartitionKSubsets(int[] nums, int k) {
         int sum = 0;
         boolean[] used = new boolean[nums.length];
+        Arrays.sort(nums);
+        reverse(nums);
         for(int num : nums){
             sum += num;
         }
@@ -22,55 +24,14 @@ class Solution {
         }
         return false;
     }
+    private void reverse(int[] nums) {
+        int left = 0, right = nums.length - 1;
+        while (left < right) {
+            int temp = nums[left];
+            nums[left] = nums[right];
+            nums[right] = temp;
+            left++;
+            right--;
+        }
+    }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// class Solution {
-//     int target;
-  
-//     public boolean canPartitionKSubsets(int[] nums, int k) {
-//         int sum = 0;
-//         for(int n : nums){
-//             sum += n;
-//         }
-//         if(sum%k != 0)
-//             return false;
-        
-//         target = sum / k;
-//         boolean[] used = new boolean[nums.length];
-//         return backtrack(nums, 0, k, 0, used);
-//     }
-
-//     private boolean backtrack(int[] nums, int i, int k, int subsetSum, boolean[] used){
-//         if(k == 0)
-//             return true;
-//         if(subsetSum == target)
-//             return backtrack(nums, 0, k-1, 0, used);
-
-//         for(int j = i; j < nums.length; j++){
-//             if(j > 0 && !used[j-1] && nums[j] == nums[j-1])
-//                 continue;
-//             if(used[j] || subsetSum + nums[j] > target)
-//                 continue;
-            
-//             used[j] = true;
-//             if(backtrack(nums, j+1, k, subsetSum + nums[j], used))
-//                 return true;
-
-//             used[j] = false;        
-//         }
-//         return false;        
-//     }
-// }
