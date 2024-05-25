e9f1767dbdd00eb826076d177b1c96f724f76c15
Joey <59043772+pan-zhao-yu@users.noreply.github.com>
2024-05-25 19:02:15 +1000

    Time: 16 ms (24.72%), Space: 44.8 MB (10.73%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,24 +1,41 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
+        Map<Character, Integer> map = new HashMap<>();
         if(s.length() != t.length()) return false;
-        HashMap<Character, Integer> map = new HashMap<>();
-        for(char ch : s.toCharArray()){
-            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        for(char c : s.toCharArray()){
+            map.put(c, map.getOrDefault(c, 0) + 1);
         }
         for(char c : t.toCharArray()){
-            if(!map.containsKey(c)){
-                return false;
-            }
-            int n = map.get(c) - 1;
-            if(n == 0){
+            if(!map.containsKey(c)) return false;
+            if(map.get(c) - 1 == 0){
                 map.remove(c);
             }else{
-                map.put(c, n);
+                map.put(c, map.get(c) - 1);
             }
         }
-            return map.isEmpty();
+        return map.isEmpty();
     }
 }
+
+
+
+
+// class Solution {
+//     public boolean isAnagram(String s, String t) {
+//         if(s.length() != t.length()) return false;
+        
+//         int[] str = new int[26];
+        
+//         for(int i = 0; i < s.length(); i++){
+//             str[s.charAt(i) - 'a'] ++;
+//             str[t.charAt(i) - 'a'] --;
+//         }
+//         for(int n : str){
+//             if(n != 0) return false;
+//         }
+//         return true;
+//     }
+// }
 
         // if(s.length() != t.length()){
         //     return false;
@@ -28,3 +45,26 @@ class Solution {
         // Arrays.sort(str1);
         // Arrays.sort(str2);
         // return Arrays.equals(str1, str2);
+
+
+// class Solution {
+//     public boolean isAnagram(String s, String t) {
+//         if(s.length() != t.length()) return false;
+//         HashMap<Character, Integer> map = new HashMap<>();
+//         for(char ch : s.toCharArray()){
+//             map.put(ch, map.getOrDefault(ch, 0) + 1);
+//         }
+//         for(char c : t.toCharArray()){
+//             if(!map.containsKey(c)){
+//                 return false;
+//             }
+//             int n = map.get(c) - 1;
+//             if(n == 0){
+//                 map.remove(c);
+//             }else{
+//                 map.put(c, n);
+//             }
+//         }
+//             return map.isEmpty();
+//     }
+// }
