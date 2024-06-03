b9b709797a493cbc15a44814a6d10bde3b8d2bc5
Joey <59043772+pan-zhao-yu@users.noreply.github.com>
2024-06-04 04:04:51 +1000

    Time: 17 ms (57.08%), Space: 44.7 MB (60.35%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,40 +1,35 @@
-
-
-
 class Solution {
     public String minWindow(String s, String t) {
-        HashMap <Character, Integer> tMap =  new HashMap<>();
-        
-        for(char x : t.toCharArray()){
-            tMap.put(x, tMap.getOrDefault(x,0) + 1);
+        if (s == null || t == null || s.length() < t.length()) {
+            return "";
         }
-        
-        int matched = 0;
+        Map<Character, Integer> tMap = new HashMap<>();
+        for(char c : t.toCharArray()){
+            tMap.put(c, tMap.getOrDefault(c, 0) + 1);
+        }
         int left = 0;
-        int minLen = s.length() + 1;
         int subStr = 0;
-        
-        for(int endWindow = 0; endWindow < s.length(); endWindow++){
-            char right = s.charAt(endWindow);
+        int matched = 0;
+        int minLen = Integer.MAX_VALUE;
+        for(int end = 0; end < s.length(); end++){
+            char right = s.charAt(end);
             if(tMap.containsKey(right)){
-                tMap.put(right, tMap.get(right)-1);
+                tMap.put(right, tMap.get(right) - 1);
                 if(tMap.get(right) == 0) matched++;
             }
-            
             while(matched == tMap.size()){
-                if(minLen > endWindow - left + 1){
-                    minLen = endWindow - left + 1;
+                if(minLen > end - left + 1){
+                    minLen = end - left + 1;
                     subStr = left;
                 }
-                
                 char deleted = s.charAt(left++);
                 if(tMap.containsKey(deleted)){
-                    if(tMap.get(deleted) == 0)matched--;
+                    if(tMap.get(deleted) == 0) matched--;
                     tMap.put(deleted, tMap.get(deleted) + 1);
                 }
             }
-            
         }
         return minLen > s.length() ? "" : s.substring(subStr, subStr + minLen);
     }
 }
+
