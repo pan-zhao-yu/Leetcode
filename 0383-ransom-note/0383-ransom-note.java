6d4229dea1feaa8d0e44ad3db4c2f65b5c14787f
Joey <59043772+pan-zhao-yu@users.noreply.github.com>
2024-06-05 00:30:07 +1000

    Time: 10 ms (58.52%), Space: 45 MB (44.70%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -1,20 +1,15 @@
-public class Solution {
+class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
-        HashMap<Character, Integer> charCountMap = new HashMap<>();
-
-        // Count characters in the magazine
-        for (char c : magazine.toCharArray()) {
-            charCountMap.put(c, charCountMap.getOrDefault(c, 0) + 1);
+        Map<Character, Integer> magaMap = new HashMap<>();
+        for(char c : magazine.toCharArray()){
+            magaMap.put(c, magaMap.getOrDefault(c, 0) + 1);
         }
-
-        // Check characters in the ransom note
-        for (char c : ransomNote.toCharArray()) {
-            if (!charCountMap.containsKey(c) || charCountMap.get(c) == 0) {
+        for(char c : ransomNote.toCharArray()){
+            if(!magaMap.containsKey(c) || magaMap.get(c) == 0){
                 return false;
             }
-            charCountMap.put(c, charCountMap.get(c) - 1);
+            magaMap.put(c, magaMap.get(c) - 1);
         }
-
         return true;
     }
 }
