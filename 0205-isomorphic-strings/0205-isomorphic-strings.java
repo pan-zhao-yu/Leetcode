363db1cd70abb7e57a36ad4b753c49c6e8cbfb29
Joey <59043772+pan-zhao-yu@users.noreply.github.com>
2024-06-05 17:31:03 +1000

    Time: 18 ms (20.67%), Space: 41.9 MB (93.97%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -1,31 +1,29 @@
-public class Solution {
+class Solution {
     public boolean isIsomorphic(String s, String t) {
-        if (s.length() != t.length()) return false;
-
-        HashMap<Character, Character> sToTMap = new HashMap<>();
-        HashMap<Character, Character> tToSMap = new HashMap<>();
-
-        for (int i = 0; i < s.length(); i++) {
-            char sChar = s.charAt(i);
-            char tChar = t.charAt(i);
-
-            if (sToTMap.containsKey(sChar)) {
-                if (sToTMap.get(sChar) != tChar) {
+        if(s.length() != t.length()) return false;
+        Map<Character, Character> sToTMap = new HashMap<>();
+        Map<Character, Character> tToSMap = new HashMap<>();
+        for(int i = 0; i < s.length(); i++){
+            char charS = s.charAt(i);
+            char charT = t.charAt(i);
+            if(sToTMap.containsKey(charS)){
+                if(!sToTMap.get(charS).equals(charT)){
                     return false;
                 }
-            } else {
-                sToTMap.put(sChar, tChar);
+            }else{
+                sToTMap.put(charS, charT);
             }
-
-            if (tToSMap.containsKey(tChar)) {
-                if (tToSMap.get(tChar) != sChar) {
+            if(tToSMap.containsKey(charT)){
+                if(!tToSMap.get(charT).equals(charS)){
                     return false;
                 }
-            } else {
-                tToSMap.put(tChar, sChar);
+            }else{
+                tToSMap.put(charT, charS);
             }
         }
-
         return true;
     }
 }
+
+
+
