85a2908ac2316618dd6702cf4efd2c1089d8de6e
Joey <59043772+pan-zhao-yu@users.noreply.github.com>
2024-06-08 21:20:41 +1000

    Time: 0 ms (100.00%), Space: 41.1 MB (53.95%) - LeetHub

--- 0225-implement-stack-using-queues/0225-implement-stack-using-queues.java
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.java
@@ -1,26 +1,26 @@
 class MyStack {
-    private Queue<Integer> queue;
+    Queue<Integer> q;
     public MyStack() {
-        queue = new LinkedList<>();
+        this.q = new LinkedList<>();
     }
     
     public void push(int x) {
-        queue.offer(x);
-        for(int i = 1; i < queue.size(); i++){
-            queue.offer(queue.poll());
+        q.add(x);
+        for(int i = 0; i < q.size() - 1; i++){
+            q.add(q.remove());
         }
     }
     
     public int pop() {
-        return queue.poll();
+        return q.remove();
     }
     
     public int top() {
-        return queue.peek();
+        return q.peek();
     }
     
     public boolean empty() {
-        return queue.isEmpty();
+        return q.isEmpty();
     }
 }
 
@@ -32,46 +32,3 @@ class MyStack {
  * int param_3 = obj.top();
  * boolean param_4 = obj.empty();
  */
-
-/** Two pointer approach*/
-// class MyStack {
-//     private Queue<Integer> queue1;
-//     private Queue<Integer> queue2;
-//     int top;
-//     public MyStack() {
-//         queue1 = new LinkedList<>();
-//         queue2 = new LinkedList<>();
-//         top = -1;
-//     }
-    
-//     public void push(int x) {
-//         queue2.offer(x);
-//         while(!queue1.isEmpty()){
-//             queue2.offer(queue1.poll());
-//         }
-//         Queue<Integer> temp = queue1;
-//         queue1 = queue2;
-//         queue2 = temp;
-//         top = queue1.peek();
-//     }
-    
-//     public int pop() {
-//         if(queue1.isEmpty()) return -1;
-//         int popped = queue1.poll();
-//         if(!queue1.isEmpty()){
-//             top = queue1.peek();
-//         }else{
-//             top = -1;
-//         }
-//         return popped;
-        
-//     }
-    
-//     public int top() {
-//         return top;
-//     }
-    
-//     public boolean empty() {
-//         return queue1.isEmpty();
-//     }
-// }
