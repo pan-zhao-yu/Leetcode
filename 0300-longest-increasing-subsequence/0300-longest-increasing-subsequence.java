75e893838770082d141de6f77d90f515e869652a
Joey <59043772+pan-zhao-yu@users.noreply.github.com>
2024-06-10 11:58:57 +1000

    Time: 44 ms (45.70%), Space: 43.9 MB (68.71%) - LeetHub

---
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -0,0 +1,20 @@
+class Solution {
+//Dp, O(n^2)
+    public int lengthOfLIS(int[] nums) {
+        if (nums.length == 1) return 1;
+
+        int[] LIS = new int[nums.length];
+        Arrays.fill(LIS, 1);
+        int maximumSoFar = 1;
+
+        for (int i = nums.length - 1; i >= 0; i--) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] < nums[j]) {
+                    LIS[i] = Math.max(1 + LIS[j], LIS[i]);
+                }
+            }
+            maximumSoFar = Math.max(maximumSoFar, LIS[i]);
+        }
+        return maximumSoFar;
+    }
+}
