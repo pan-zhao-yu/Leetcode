51808972800122158617956654c0f48d988769da
Joey <59043772+pan-zhao-yu@users.noreply.github.com>
2024-06-10 11:34:41 +1000

    Time: 0 ms (100.00%), Space: 43.9 MB (90.69%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -9,21 +9,16 @@
  *     }
  * }
  */
-
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        ListNode fast = head;
         ListNode slow = head;
-        
+        ListNode fast = head;
         while(fast != null && fast.next != null){
-
             fast = fast.next.next;
             slow = slow.next;
-            
-            if(fast == slow){
+            if(slow == fast){
                 return true;
             }
-            
         }
         return false;
     }
