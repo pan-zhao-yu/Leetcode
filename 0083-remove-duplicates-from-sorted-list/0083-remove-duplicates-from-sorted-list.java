3fac49675a42855ed121cd67abecf22ca249f340
Joey <59043772+pan-zhao-yu@users.noreply.github.com>
2024-06-12 16:42:42 +1000

    Time: 0 ms (100.00%), Space: 43.8 MB (63.04%) - LeetHub

--- 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.java
+++ 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.java
@@ -17,7 +17,6 @@ class Solution {
             }
             if(result.val == result.next.val){
                 result.next = result.next.next;
-                
             }else{
                 result = result.next;
             }
@@ -25,3 +24,22 @@ class Solution {
         return head;
     }
 }
+
+
+// class Solution {
+//     public ListNode deleteDuplicates(ListNode head) {
+//         ListNode result = head;
+//         while(result != null){
+//             if(result.next == null){
+//                 break;
+//             }
+//             if(result.val == result.next.val){
+//                 result.next = result.next.next;
+                
+//             }else{
+//                 result = result.next;
+//             }
+//         }
+//         return head;
+//     }
+// }
