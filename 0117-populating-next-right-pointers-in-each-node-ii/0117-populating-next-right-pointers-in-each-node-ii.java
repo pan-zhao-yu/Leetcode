75592ff08a5d73961c04dfc265ab4bb928e19fe7
Joey <59043772+pan-zhao-yu@users.noreply.github.com>
2024-06-18 03:10:23 +1000

    Time: 0 ms (100.00%), Space: 43.6 MB (97.95%) - LeetHub

--- 0117-populating-next-right-pointers-in-each-node-ii/0117-populating-next-right-pointers-in-each-node-ii.java
+++ 0117-populating-next-right-pointers-in-each-node-ii/0117-populating-next-right-pointers-in-each-node-ii.java
@@ -24,20 +24,48 @@ class Node {
 class Solution {
     public Node connect(Node root) {
         if(root == null) return root;
-        Queue<Node> queue = new LinkedList<>();
-        queue.add(root);
-        while(!queue.isEmpty()){
-            int size = queue.size();
-            for(int i = 0; i < size; i++){
-                if(queue.peek().left != null) queue.add(queue.peek().left);
-                if(queue.peek().right != null) queue.add(queue.peek().right);
-                if(i == size - 1){
-                    queue.poll().next = null;
-                }else{
-                    queue.poll().next = queue.peek();
-                }
+        Node head = new Node(0);
+        Node curr = root;
+        Node tail = head;
+        while(curr != null){
+            if(curr.left != null){
+                tail.next = curr.left;
+                tail = tail.next;
+            }
+            if(curr.right != null){
+                tail.next = curr.right;
+                tail = tail.next;
+            }
+            curr = curr.next;
+            if(curr == null){
+                curr = head.next;
+                head.next = null;
+                tail = head;
             }
         }
         return root;
     }
 }
+
+
+
+// class Solution {
+//     public Node connect(Node root) {
+//         if(root == null) return root;
+//         Queue<Node> queue = new LinkedList<>();
+//         queue.add(root);
+//         while(!queue.isEmpty()){
+//             int size = queue.size();
+//             for(int i = 0; i < size; i++){
+//                 if(queue.peek().left != null) queue.add(queue.peek().left);
+//                 if(queue.peek().right != null) queue.add(queue.peek().right);
+//                 if(i == size - 1){
+//                     queue.poll().next = null;
+//                 }else{
+//                     queue.poll().next = queue.peek();
+//                 }
+//             }
+//         }
+//         return root;
+//     }
+// }
