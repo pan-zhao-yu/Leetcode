14db2904d61660e6d01d7916eb32b0c578f7a9ff
Joey <59043772+pan-zhao-yu@users.noreply.github.com>
2024-06-18 17:07:14 +1000

    Time: 0 ms (100.00%), Space: 40.9 MB (57.22%) - LeetHub

--- 0100-same-tree/0100-same-tree.java
+++ 0100-same-tree/0100-same-tree.java
@@ -16,20 +16,11 @@
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
         return dfs(p, q);
-        
     }
-    
     private boolean dfs(TreeNode p, TreeNode q){
-        if(p == null && q == null){
-            return true;
-        }
-        if(p == null || q == null){
-            return false;
-        }
-        if(p.val != q.val){
-            return false;
-        }
-        
+        if(p == null && q == null) return true;
+        if(p == null || q == null) return false;
+        if(p.val != q.val) return false;
         return dfs(p.left, q.left) && dfs(p.right, q.right);
     }
 }
