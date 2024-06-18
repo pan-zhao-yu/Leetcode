2dc67a5b6cc328bc7df439af6de6eadf856a87c0
Joey <59043772+pan-zhao-yu@users.noreply.github.com>
2024-06-18 17:34:43 +1000

    Time: 1 ms (66.55%), Space: 42.5 MB (6.63%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
@@ -16,30 +16,50 @@
 class Solution {
     public List<Integer> rightSideView(TreeNode root) {
         List<Integer> result = new ArrayList<>();
-        Queue<TreeNode> queue = new LinkedList<>();
-        
-        if(root == null){
-            return result;
-        }
-        queue.offer(root);
-        
-        while(!queue.isEmpty()){
-            int levelSize = queue.size();
-            for(int i = 0; i < levelSize; i++){
-                TreeNode current = queue.poll();
-                if(i == levelSize -1){
-                    result.add(current.val);
-                }
-                
-                if(current.left != null){
-                    queue.offer(current.left);
-                }
-                if(current.right != null){
-                    queue.offer(current.right);
-                }
+        if(root == null) return result;
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        while(!q.isEmpty()){
+            int size = q.size();
+            for(int i = 0; i < size; i++){
+                TreeNode curr = q.poll();
+                if(i == size - 1) result.add(curr.val);
+                if(curr.left != null) q.add(curr.left);
+                if(curr.right != null) q.add(curr.right);
             }
-
         }
         return result;
     }
 }
+
+
+// class Solution {
+//     public List<Integer> rightSideView(TreeNode root) {
+//         List<Integer> result = new ArrayList<>();
+//         Queue<TreeNode> queue = new LinkedList<>();
+        
+//         if(root == null){
+//             return result;
+//         }
+//         queue.offer(root);
+        
+//         while(!queue.isEmpty()){
+//             int levelSize = queue.size();
+//             for(int i = 0; i < levelSize; i++){
+//                 TreeNode current = queue.poll();
+//                 if(i == levelSize -1){
+//                     result.add(current.val);
+//                 }
+                
+//                 if(current.left != null){
+//                     queue.offer(current.left);
+//                 }
+//                 if(current.right != null){
+//                     queue.offer(current.right);
+//                 }
+//             }
+
+//         }
+//         return result;
+//     }
+// }
