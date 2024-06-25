92f000d0f000bd96b5342f4eafbcc613252694ac
Joey <59043772+pan-zhao-yu@users.noreply.github.com>
2024-06-26 00:57:13 +1000

    Time: 3 ms (47.86%), Space: 44.3 MB (60.99%) - LeetHub

--- 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.java
+++ 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.java
@@ -16,21 +16,43 @@
 class Solution {
     public int findBottomLeftValue(TreeNode root) {
         if(root == null) return 0;
+        int result = 0;
         Queue<TreeNode> q = new LinkedList<>();
-        q.offer(root);
-        int ans = root.val;
-        
+        q.add(root);
         while(!q.isEmpty()){
-            TreeNode curr = q.poll();
-            ans = curr.val;
-            
-            if(curr.right != null){
-                q.offer(curr.right);
-            }
-            if(curr.left != null){
-                q.offer(curr.left);
+            int size = q.size();
+            for(int i = 0; i < size; i++){
+                TreeNode curr = q.poll();
+                if(i == 0){
+                    result = curr.val;
+                }
+                if(curr.left != null) q.add(curr.left);
+                if(curr.right != null) q.add(curr.right);
             }
         }
-        return ans;
+        return result;
     }
 }
+
+
+// class Solution {
+//     public int findBottomLeftValue(TreeNode root) {
+//         if(root == null) return 0;
+//         Queue<TreeNode> q = new LinkedList<>();
+//         q.offer(root);
+//         int ans = root.val;
+        
+//         while(!q.isEmpty()){
+//             TreeNode curr = q.poll();
+//             ans = curr.val;
+            
+//             if(curr.right != null){
+//                 q.offer(curr.right);
+//             }
+//             if(curr.left != null){
+//                 q.offer(curr.left);
+//             }
+//         }
+//         return ans;
+//     }
+// }
