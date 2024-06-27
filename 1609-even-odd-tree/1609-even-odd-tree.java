f05dcff91130997534449980d7aa901ce6226251
Joey <59043772+pan-zhao-yu@users.noreply.github.com>
2024-06-27 15:51:51 +1000

    Time: 9 ms (90.80%), Space: 66 MB (53.62%) - LeetHub

--- 1609-even-odd-tree/1609-even-odd-tree.java
+++ 1609-even-odd-tree/1609-even-odd-tree.java
@@ -16,22 +16,18 @@
 class Solution {
     public boolean isEvenOddTree(TreeNode root) {
         if(root == null) return true;
-        boolean even = true;
         Queue<TreeNode> q = new LinkedList<>();
         q.offer(root);
+        boolean even = true;
         while(!q.isEmpty()){
             int size = q.size();
             int prev = even ? Integer.MIN_VALUE : Integer.MAX_VALUE;
             for(int i = 0; i < size; i++){
                 TreeNode curr = q.poll();
                 if(even){
-                    if(curr.val % 2 == 0 || curr.val <= prev){
-                        return false;
-                    }
+                    if(curr.val % 2 == 0 || curr.val <= prev) return false;
                 }else{
-                    if(curr.val % 2 != 0 || curr.val >= prev){
-                        return false;
-                    }
+                    if(curr.val % 2 == 1 || curr.val >= prev) return false;
                 }
                 prev = curr.val;
                 if(curr.left != null) q.offer(curr.left);
