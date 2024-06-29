60466e7b6be8ac5385e866c96f6320a60ebe9205
Joey <59043772+pan-zhao-yu@users.noreply.github.com>
2024-06-29 19:24:19 +1000

    Time: 2 ms (87.27%), Space: 55.9 MB (28.10%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,11 +1,10 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
+        int[] res = new int[nums.length];
         int[] pre = new int[nums.length];
         int[] post = new int[nums.length];
-        int[] ans = new int[nums.length];
         pre[0] = 1;
         post[nums.length - 1] = 1;
-        
         for(int i = 1; i < nums.length; i++){
             pre[i] = pre[i - 1] * nums[i - 1];
         }
@@ -13,31 +12,8 @@ class Solution {
             post[i] = post[i + 1] * nums[i + 1];
         }
         for(int i = 0; i < nums.length; i++){
-            ans[i] = post[i] * pre[i];
+            res[i] = pre[i] * post[i];
         }
-        return ans;
+        return res;
     }
 }
-
-
-// class Solution {
-//     public int[] productExceptSelf(int[] nums) {
-//         int pre[] = new int[nums.length];
-//         int post[] = new int[nums.length];
-//         int answer[] = new int[nums.length];
-//         pre[0] = 1;
-//         post[nums.length - 1] = 1;
-        
-//         for(int i = 1; i < nums.length; i++){
-//             pre[i] = pre[i - 1] * nums[i - 1];
-//         }
-//         for(int i = nums.length - 2; i>= 0; i--){
-//             post[i] = post[i + 1] * nums[i+1];
-//         }
-
-//         for(int i = 0; i < nums.length; i++){
-//             answer[i] = pre[i] * post[i];
-//         }
-//         return answer;
-//     }
-// }
