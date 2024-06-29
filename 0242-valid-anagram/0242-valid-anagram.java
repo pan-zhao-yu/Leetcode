53904bf87918ef39642e42219b32d73f4275c638
Joey <59043772+pan-zhao-yu@users.noreply.github.com>
2024-06-29 16:40:30 +1000

    Time: 17 ms (20.10%), Space: 44.6 MB (29.23%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,70 +1,16 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
         Map<Character, Integer> map = new HashMap<>();
-        if(s.length() != t.length()) return false;
         for(char c : s.toCharArray()){
             map.put(c, map.getOrDefault(c, 0) + 1);
         }
         for(char c : t.toCharArray()){
-            if(!map.containsKey(c)) return false;
-            if(map.get(c) - 1 == 0){
-                map.remove(c);
-            }else{
+            if(map.containsKey(c) && map.get(c) > 0){
                 map.put(c, map.get(c) - 1);
+            }else{
+                return false;
             }
         }
-        return map.isEmpty();
+        return !map.values().stream().anyMatch(value -> value > 0);
     }
 }
-
-
-
-
-// class Solution {
-//     public boolean isAnagram(String s, String t) {
-//         if(s.length() != t.length()) return false;
-        
-//         int[] str = new int[26];
-        
-//         for(int i = 0; i < s.length(); i++){
-//             str[s.charAt(i) - 'a'] ++;
-//             str[t.charAt(i) - 'a'] --;
-//         }
-//         for(int n : str){
-//             if(n != 0) return false;
-//         }
-//         return true;
-//     }
-// }
-
-        // if(s.length() != t.length()){
-        //     return false;
-        // }
-        // char[] str1 = s.toLowerCase().toCharArray();
-        // char[] str2 = t.toLowerCase().toCharArray();
-        // Arrays.sort(str1);
-        // Arrays.sort(str2);
-        // return Arrays.equals(str1, str2);
-
-
-// class Solution {
-//     public boolean isAnagram(String s, String t) {
-//         if(s.length() != t.length()) return false;
-//         HashMap<Character, Integer> map = new HashMap<>();
-//         for(char ch : s.toCharArray()){
-//             map.put(ch, map.getOrDefault(ch, 0) + 1);
-//         }
-//         for(char c : t.toCharArray()){
-//             if(!map.containsKey(c)){
-//                 return false;
-//             }
-//             int n = map.get(c) - 1;
-//             if(n == 0){
-//                 map.remove(c);
-//             }else{
-//                 map.put(c, n);
-//             }
-//         }
-//             return map.isEmpty();
-//     }
-// }
