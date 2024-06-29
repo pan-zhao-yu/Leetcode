d288fd3502bb25989c48af4421e3b6bcf6ba470f
Joey <59043772+pan-zhao-yu@users.noreply.github.com>
2024-06-29 18:20:43 +1000

    Time: 13 ms (71.87%), Space: 48.5 MB (44.49%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,43 +1,20 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
         Map<Integer, Integer> map = new HashMap<>();
-        for(int i = 0; i < nums.length; i++){
-            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+        PriorityQueue<Map.Entry<Integer, Integer>> minHeap = new PriorityQueue<>((a, b) -> a.getValue() - b.getValue());
+        for(int n : nums){
+            map.put(n, map.getOrDefault(n, 0) + 1);
+        }for(Map.Entry<Integer, Integer> entry : map.entrySet()){
+            minHeap.add(entry);
+            if(minHeap.size() > k){
+                minHeap.poll();
+            }
         }
-        
-        PriorityQueue<Integer> heap = new PriorityQueue<>((n1, n2) -> map.get(n1) - map.get(n2));
-        for(int num : map.keySet()){
-            heap.add(num);
-            if(heap.size() > k) heap.poll();
+        int[] topK = new int[k];
+        int index = 0;
+        while(!minHeap.isEmpty()){
+            topK[index++] = minHeap.poll().getKey();        
         }
-        
-        int[] res = new int[k];
-        for(int i = k - 1; i >= 0; --i){
-            res[i] = heap.poll();
-        }
-        return res;
+        return topK;
     }
 }
-
-
-
-// class Solution {
-//     public int[] topKFrequent(int[] nums, int k) {
-//         Map<Integer, Integer> map = new HashMap<>();
-//         for (int num : nums) {
-//             map.put(num, map.getOrDefault(num, 0) + 1);
-//         }
-
-//         PriorityQueue<Integer> heap = new PriorityQueue<>((n1, n2) -> map.get(n1) - map.get(n2));
-//         for (int num : map.keySet()) {
-//             heap.add(num);
-//             if (heap.size() > k) heap.poll();
-//         }
-
-//         int[] topK = new int[k];
-//         for (int i = k - 1; i >= 0; --i) {
-//             topK[i] = heap.poll();
-//         }
-//         return topK;
-//     }
-// }
