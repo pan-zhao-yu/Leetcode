cc953f20e7425fd435143b5f4ea8b328dcadc359
Joey <59043772+pan-zhao-yu@users.noreply.github.com>
2024-07-05 01:18:12 +1000

    Time: 0 ms (100.00%), Space: 41.8 MB (82.48%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -4,20 +4,16 @@ class Solution {
         int col = matrix[0].length;
         int left = 0;
         int right = row * col - 1;
-        
         while(left <= right){
             int mid = left + (right - left) / 2;
-            int midValue = matrix[mid / col][mid % col];
-            if(target == midValue){
-                return true;
-            }else if(target < midValue){
+            if(matrix[mid / col][mid % col] < target){
+                left = mid + 1;
+            }else if(matrix[mid / col][mid % col] > target){
                 right = mid - 1;
             }else{
-                left = mid + 1;
+                return true;
             }
-            
         }
         return false;
-        
     }
 }
