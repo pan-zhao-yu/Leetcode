fe369174ebff6cdd22455465f3204b4482f4ae78
Joey <59043772+pan-zhao-yu@users.noreply.github.com>
2024-07-23 20:28:07 +1000

    Time: 1 ms (93.77%), Space: 44.4 MB (58.02%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,44 +1,22 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
-        backtrack(result, new ArrayList<>(), nums);
+        boolean[] used = new boolean[nums.length];
+        backtrack(result, new ArrayList<>(), nums, used);
         return result;
     }
     
-    private void backtrack(List<List<Integer>> result, List<Integer> temp, int[] nums){
+    private void backtrack(List<List<Integer>> result, List<Integer> temp, int[] nums, boolean[] used){
         if(temp.size() == nums.length){
-            result.add(new ArrayList(temp));
-        }else{
-            for(int i = 0; i < nums.length; i++){
-                if(temp.contains(nums[i])) continue;
-                temp.add(nums[i]);
-                backtrack(result, temp, nums);
-                temp.remove(temp.size() - 1);
-            }
+            result.add(new ArrayList<>(temp));
         }
-        
+        for(int i = 0; i < nums.length; i++){
+            if(used[i]) continue;
+            temp.add(nums[i]);
+            used[i] = true;
+            backtrack(result, temp, nums, used);
+            temp.remove(temp.size() - 1);
+            used[i] = false;
+        }
     }
 }
-
-
-
-// class Solution {
-//     public List<List<Integer>> permute(int[] nums) {
-//         List<List<Integer>> list = new ArrayList<>();
-//         backtrack(list, new ArrayList<>(), nums);
-//         return list;
-//     }
-    
-//     private void backtrack(List<List<Integer>> list, List<Integer> temp, int[] nums){
-//         if(temp.size() == nums.length){
-//             list.add(new ArrayList(temp));
-//         }else{
-//             for(int i = 0; i < nums.length; i++){
-//                 if(temp.contains(nums[i])) continue;
-//                 temp.add(nums[i]);
-//                 backtrack(list, temp, nums);
-//                 temp.remove(temp.size() - 1);
-//             }
-//         }
-//     }
-// }
