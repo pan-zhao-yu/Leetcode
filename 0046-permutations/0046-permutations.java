6f55c7d99ddf0cad553489991d8d922dcf65e0e1
Joey <59043772+pan-zhao-yu@users.noreply.github.com>
2024-07-24 20:13:20 +1000

    Time: 2 ms (42.10%), Space: 44.2 MB (86.62%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -6,14 +6,14 @@ class Solution {
     }
     
     private void backtrack(List<List<Integer>> result, List<Integer> temp, int[] nums){
-        if(temp.size() == nums.length) result.add(new ArrayList<>(temp));
-        else{
-            for(int i = 0; i < nums.length; i++){
-                if(temp.contains(nums[i])) continue;
-                temp.add(nums[i]);
-                backtrack(result, temp, nums);
-                temp.remove(temp.size() - 1);
-            }
+        if(temp.size() == nums.length){
+            result.add(new ArrayList<>(temp));
+        }
+        for(int i = 0; i < nums.length; i++){
+            if(temp.contains(nums[i])) continue;
+            temp.add(nums[i]);
+            backtrack(result, temp, nums);
+            temp.remove(temp.size() - 1);
         }
     }
 }
