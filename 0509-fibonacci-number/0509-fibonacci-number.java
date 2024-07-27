0f9da5da32f722d410917188296ff1f5de79088b
Joey <59043772+pan-zhao-yu@users.noreply.github.com>
2024-07-27 17:53:43 +1000

    Time: 0 ms (100.00%), Space: 40.4 MB (29.21%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,8 +1,38 @@
+//      **Optimised DP**
 class Solution {
     public int fib(int n) {
-        if(n <= 1){
-            return n;
+        if(n <= 1) return n;
+        int a = 0;
+        int b = 1;
+        for(int i = 2; i <= n; i++){
+            int sum = a + b;
+            a = b;
+            b = sum;
         }
-        return fib(n - 1) + fib(n - 2);
+        return b;
     }
 }
+
+//      **DP**
+// class Solution {
+//     public int fib(int n) {
+//         if(n <= 1) return n;
+//         int[] dp = new int[n + 1];
+//         dp[0] = 0;
+//         dp[1] = 1;
+//         for(int i = 2; i <= n; i++){
+//             dp[i] = dp[i - 1] + dp[i - 2];
+//         }
+//         return dp[n];
+//     }
+// }
+
+//      **Recursive**
+// class Solution {
+//     public int fib(int n) {
+//         if(n <= 1){
+//             return n;
+//         }
+//         return fib(n - 1) + fib(n - 2);
+//     }
+// }
