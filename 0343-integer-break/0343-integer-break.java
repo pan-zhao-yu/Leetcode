d39364569d9a77fca1a175b88daabbd29cebead3
Joey <59043772+pan-zhao-yu@users.noreply.github.com>
2024-08-01 01:32:36 +1000

    Time: 1 ms (44.94%), Space: 40.1 MB (75.00%) - LeetHub

--- 0343-integer-break/0343-integer-break.java
+++ 0343-integer-break/0343-integer-break.java
@@ -1,14 +1,20 @@
-public class Solution {
+class Solution {
     public int integerBreak(int n) {
-        if(n==2) return 1;
-        if(n==3) return 2;
-        int product = 1;
-        while(n>4){
-            product*=3;
-            n-=3;
-        }
-        product*=n;
+        if (n == 2) return 1; // Special case
+        if (n == 3) return 2; // Special case
         
-        return product;
+        int[] dp = new int[n + 1];
+        dp[1] = 1; // This value won't be used for n >= 4
+        
+        for (int i = 2; i <= n; i++) {
+            for (int j = 1; j < i; j++) {
+                dp[i] = Math.max(dp[i], Math.max(j * (i - j), j * dp[i - j]));
+            }
+        }
+        
+        return dp[n];
     }
 }
+
+//dp[i] = j * dp[i - j];
+//dp[i] = j * (i - j);
