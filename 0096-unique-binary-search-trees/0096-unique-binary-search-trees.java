9cd7447c3dce06786dbefe31c5945c183440a52d
Joey <59043772+pan-zhao-yu@users.noreply.github.com>
2024-08-02 02:44:16 +1000

    Time: 2583 ms (8.65%), Space: 40.7 MB (9.99%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.java
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.java
@@ -1,14 +1,32 @@
 class Solution {
+    
     public int numTrees(int n) {
-        int[] dp = new int[n + 1];
-        dp[0] = 1;
-        dp[1] = 1;
-        for(int i = 2; i <= n; i++){
-            for(int j = 1; j <= i; j++){
-                dp[i] += dp[j - 1] * dp[i - j];
-            }
+        int x = 0;
+        if(n == 0){
+            return 1;
         }
-        return dp[n];
+        if(n == 1) return 1;
+        
+        for(int i = 0; i <= n - 1; i++){
+            x += numTrees(i) * numTrees(n - 1 - i);
+        }
+        return x;
     }
 }
+
+
+
+// class Solution {
+//     public int numTrees(int n) {
+//         int[] dp = new int[n + 1];
+//         dp[0] = 1;
+//         dp[1] = 1;
+//         for(int i = 2; i <= n; i++){
+//             for(int j = 1; j <= i; j++){
+//                 dp[i] += dp[j - 1] * dp[i - j];
+//             }
+//         }
+//         return dp[n];
+//     }
+// }
 //dp[i] = dp[j - 1] * dp[i - j];
