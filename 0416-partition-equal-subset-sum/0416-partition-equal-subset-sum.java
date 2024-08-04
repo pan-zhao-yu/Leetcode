7cd96369bc9185ab1aea78e8ef308302d9c286d4
Joey <59043772+pan-zhao-yu@users.noreply.github.com>
2024-08-05 01:20:11 +1000

    Time: 29 ms (77.93%), Space: 56.3 MB (39.22%) - LeetHub

---
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean canPartition(int[] nums) {
+        int sum = 0;
+        for (int n : nums) {
+            sum += n;
+        }
+        if (sum % 2 != 0) return false;
+        
+        int target = sum / 2;
+        int[][] dp = new int[nums.length + 1][target + 1];
+        
+        for (int i = 1; i <= nums.length; i++) {
+            for (int j = 1; j <= target; j++) {
+                if (j >= nums[i - 1]) {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - nums[i - 1]] + nums[i - 1]);
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+        
+        return dp[nums.length][target] == target;
+    }
+}
+
+//dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - nums[i] + nums[i]])
+//maximum item from 0 - nums[i] in size j
