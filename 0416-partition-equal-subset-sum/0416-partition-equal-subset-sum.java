fbcad0a28b5f5ed8875cbcedd3f3bcab170bd296
Joey <59043772+pan-zhao-yu@users.noreply.github.com>
2024-08-05 13:34:20 +1000

    Time: 29 ms (77.94%), Space: 56.2 MB (39.72%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,16 +1,15 @@
 class Solution {
     public boolean canPartition(int[] nums) {
         int sum = 0;
-        for (int n : nums) {
+        for(int n : nums){
             sum += n;
         }
-        if (sum % 2 != 0) return false;
-        
+        if(sum % 2 != 0) return false;
         int target = sum / 2;
         int[][] dp = new int[nums.length + 1][target + 1];
-        
-        for (int i = 1; i <= nums.length; i++) {
-            for (int j = 1; j <= target; j++) {
+
+        for(int i = 1; i < nums.length + 1; i++){
+            for(int j = 1; j < target + 1; j++){
                 if (j >= nums[i - 1]) {
                     dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - nums[i - 1]] + nums[i - 1]);
                 } else {
@@ -18,10 +17,7 @@ class Solution {
                 }
             }
         }
-        
         return dp[nums.length][target] == target;
     }
 }
-
-//dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - nums[i] + nums[i]])
-//maximum item from 0 - nums[i] in size j
+//dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - nums[i - 1]] + nums[i - 1])
