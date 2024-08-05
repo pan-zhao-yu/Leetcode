9ea345f4e4a3b66529d6efa5dd99009df6657de3
Joey <59043772+pan-zhao-yu@users.noreply.github.com>
2024-08-05 16:19:18 +1000

    Time: 18 ms (94.28%), Space: 42.2 MB (87.41%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -6,18 +6,38 @@ class Solution {
         }
         if(sum % 2 != 0) return false;
         int target = sum / 2;
-        int[][] dp = new int[nums.length + 1][target + 1];
-
-        for(int i = 1; i < nums.length + 1; i++){
-            for(int j = 1; j < target + 1; j++){
-                if (j >= nums[i - 1]) {
-                    dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - nums[i - 1]] + nums[i - 1]);
-                } else {
-                    dp[i][j] = dp[i - 1][j];
-                }
+        int[] dp = new int[target + 1];
+        for(int n : nums){
+            for(int j = target; j >= n; j--){
+                dp[j] = Math.max(dp[j], dp[j - n] + n);
             }
         }
-        return dp[nums.length][target] == target;
+        return dp[target] == target;
     }
 }
+
+
+// class Solution {
+//     public boolean canPartition(int[] nums) {
+//         int sum = 0;
+//         for(int n : nums){
+//             sum += n;
+//         }
+//         if(sum % 2 != 0) return false;
+//         int target = sum / 2;
+//         int[][] dp = new int[nums.length + 1][target + 1];
+
+//         for(int i = 1; i < nums.length + 1; i++){
+//             for(int j = 1; j < target + 1; j++){
+//                 if (j >= nums[i - 1]) {
+//                     dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - nums[i - 1]] + nums[i - 1]);
+//                 } else {
+//                     dp[i][j] = dp[i - 1][j];
+//                 }
+//             }
+//         }
+//         return dp[nums.length][target] == target;
+//     }
+// }
 //dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - nums[i - 1]] + nums[i - 1])
+//dp[j] = Math.max(dp[j], dp[j - nums[i - 1] + nums[i - 1]])
