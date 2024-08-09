99b01be49e8eb63ae9bfe5ce72d3fc76740aa8e9
Joey <59043772+pan-zhao-yu@users.noreply.github.com>
2024-08-09 17:08:43 +1000

    Time: 7 ms (47.52%), Space: 47.7 MB (46.44%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -2,11 +2,7 @@ class Solution {
     public int change(int amount, int[] coins) {
         int[][] dp = new int[coins.length + 1][amount + 1];
         for(int i = 0; i <= coins.length; i++){
-            for(int j = 0; j <= amount; j++){
-                if(j == 0){
-                    dp[i][j] = 1;
-                }
-            }
+            dp[i][0] = 1;
         }
         for(int i = 1; i <= coins.length; i++){
             for(int j = 1; j <= amount; j++){
