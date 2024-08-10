c0d06da27883e68551082d7738e543b3bab7e458
Joey <59043772+pan-zhao-yu@users.noreply.github.com>
2024-08-10 12:16:15 +1000

    Time: 3 ms (11.23%), Space: 41.5 MB (11.07%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.java
+++ 0377-combination-sum-iv/0377-combination-sum-iv.java
@@ -1,21 +1,30 @@
 class Solution {
     public int combinationSum4(int[] nums, int target) {
-        int[] dp = new int[target + 1];
+        int n = nums.length;
+        int[][] dp = new int[target + 1][n + 1];
         
-        dp[0] = 1;  // There's one way to make sum 0, by choosing nothing
+        // Initialize dp[0][j] = 1 for all j because there's one way to get sum 0 (by choosing nothing)
+        for (int j = 0; j <= n; j++) {
+            dp[0][j] = 1;
+        }
         
-        // Iterate over every possible target sum
-        for (int j = 1; j <= target; j++) {
-            for (int num : nums) {
-                if (j >= num) {
-                    dp[j] += dp[j - num];  // Add the number of ways to make sum (j - num)
+        // Fill the dp array
+        for (int i = 1; i <= target; i++) {
+            for (int j = 1; j <= n; j++) {
+                // Number of ways to form sum i using the first j elements without including nums[j-1]
+                dp[i][j] = dp[i][j-1];
+                
+                // Include the current element nums[j-1] if it fits into the sum
+                if (i >= nums[j-1]) {
+                    dp[i][j] += dp[i - nums[j-1]][n]; // Use all elements to form remaining sum
                 }
             }
         }
         
-        return dp[target];
+        return dp[target][n];
     }
 }
+
 
 
 
