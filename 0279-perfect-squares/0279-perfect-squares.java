e152c88d17883d15592d651a77d7c81fc2bfda86
Joey <59043772+pan-zhao-yu@users.noreply.github.com>
2024-08-10 13:42:22 +1000

    Time: 917 ms (5.05%), Space: 55.1 MB (5.30%) - LeetHub

---
+++ 0279-perfect-squares/0279-perfect-squares.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int numSquares(int n) {
+        List<Integer> squares = new LinkedList<>();
+        for(int i = 1; i * i <= n; i++){
+            squares.add(i * i);
+        }
+        int[][] dp = new int[squares.size() + 1][n + 1];
+        for(int i = 0; i <= squares.size(); i++){
+            dp[i][0] = 0;
+        }
+        for(int j = 0; j <= n; j++){
+            dp[0][j] = Integer.MAX_VALUE - 1;
+        }
+        for(int i = 1; i <= squares.size(); i++){
+            for(int j = 1; j <= n; j++){
+                if(j >= squares.get(i - 1)){
+                    dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - squares.get(i - 1)] + 1);
+                }else{
+                    dp[i][j] = dp[i -1][j];
+                }
+            }
+        }
+        return dp[squares.size()][n];
+    }
+}
