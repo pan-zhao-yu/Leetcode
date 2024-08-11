71b5975ca3a734df6c76bc83a0fdbf8ea60b6baf
Joey <59043772+pan-zhao-yu@users.noreply.github.com>
2024-08-11 10:58:35 +1000

    Time: 35 ms (57.85%), Space: 42.7 MB (49.72%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.java
+++ 0279-perfect-squares/0279-perfect-squares.java
@@ -1,25 +1,42 @@
 class Solution {
     public int numSquares(int n) {
-        List<Integer> squares = new LinkedList<>();
-        for(int i = 1; i * i <= n; i++){
-            squares.add(i * i);
+        int[] dp = new int[n + 1];
+        for(int i = 0; i <= n; i++){
+            dp[i] = Integer.MAX_VALUE;
         }
-        int[][] dp = new int[squares.size() + 1][n + 1];
-        for(int i = 0; i <= squares.size(); i++){
-            dp[i][0] = 0;
-        }
-        for(int j = 1; j <= n; j++){
-            dp[0][j] = Integer.MAX_VALUE - 1;
-        }
-        for(int i = 1; i <= squares.size(); i++){
-            for(int j = 1; j <= n; j++){
-                if(j >= squares.get(i - 1)){
-                    dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - squares.get(i - 1)] + 1);
-                }else{
-                    dp[i][j] = dp[i -1][j];
-                }
+        dp[0] = 0;
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j * j <= i; j++){
+                dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
             }
         }
-        return dp[squares.size()][n];
+        return dp[n];
     }
 }
+//dp[j] = Math.min(dp[j], dp[j - square] + 1);
+
+// class Solution {
+//     public int numSquares(int n) {
+//         List<Integer> squares = new LinkedList<>();
+//         for(int i = 1; i * i <= n; i++){
+//             squares.add(i * i);
+//         }
+//         int[][] dp = new int[squares.size() + 1][n + 1];
+//         for(int i = 0; i <= squares.size(); i++){
+//             dp[i][0] = 0;
+//         }
+//         for(int j = 1; j <= n; j++){
+//             dp[0][j] = Integer.MAX_VALUE - 1;
+//         }
+//         for(int i = 1; i <= squares.size(); i++){
+//             for(int j = 1; j <= n; j++){
+//                 if(j >= squares.get(i - 1)){
+//                     dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - squares.get(i - 1)] + 1);
+//                 }else{
+//                     dp[i][j] = dp[i -1][j];
+//                 }
+//             }
+//         }
+//         return dp[squares.size()][n];
+//     }
+// }
