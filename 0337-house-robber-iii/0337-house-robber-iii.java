1951d5f49d19e9576b214d7a21a3a8d13bfeddba
Joey <59043772+pan-zhao-yu@users.noreply.github.com>
2024-08-12 11:41:11 +1000

    Time: 0 ms (100.00%), Space: 44.3 MB (69.59%) - LeetHub

--- 0337-house-robber-iii/0337-house-robber-iii.java
+++ 0337-house-robber-iii/0337-house-robber-iii.java
@@ -15,21 +15,17 @@
  */
 class Solution {
     public int rob(TreeNode root) {
-        int[] result = dfs(root);
-        return Math.max(result[0], result[1]);
+        int[] result = robSub(root);
+        return Math.max(result[0],result[1]);
     }
     
-    private int[] dfs(TreeNode node){
-        if(node == null){
-            return new int[]{0,0};
-        }
-        
-        int[] left = dfs(node.left);
-        int[] right = dfs(node.right);
-        
-        int robCurr =  node.val + left[1] + right[1];
-        int skipCurr = Math.max(left[0], left[1]) + Math.max(right[0], right[1]);
-        
-        return new int[]{robCurr, skipCurr};
+    private int[] robSub(TreeNode node){
+        if(node == null) return new int[2];
+        int[] left = robSub(node.left);
+        int[] right = robSub(node.right);
+        int robThis = node.val + left[1] + right[1];
+        int notRobThis = Math.max(left[0], left[1]) + Math.max(right[0], right[1]);
+        return new int[]{robThis, notRobThis};
     }
+    
 }
