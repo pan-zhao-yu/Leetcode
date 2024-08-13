61d0c7244d40f01485b1e827ec2315596cf865f7
Joey <59043772+pan-zhao-yu@users.noreply.github.com>
2024-08-13 11:05:22 +1000

    Time: 27 ms (7.73%), Space: 57.7 MB (97.56%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -4,9 +4,12 @@ class Solution {
         dp[0][0] = -prices[0];
         dp[0][1] = 0;
         for(int i = 1; i < prices.length; i++){
-            dp[i][0] = Math.max(dp[i - 1][0], -prices[i]);
+            dp[i][0] = Math.max(-prices[i], dp[i - 1][0]);
             dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] + prices[i]);
         }
         return dp[prices.length - 1][1];
     }
 }
+
+//(hold)dp[i][0] = max(-prices[i], dp[i - 1][0]);
+//(sell)dp[i][1] = max(dp[i - 1][0] + prices[i], dp[i - 1][1]);
