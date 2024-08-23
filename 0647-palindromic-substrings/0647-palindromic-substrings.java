eb4df78b4a456e01faee212b70ae8018d8ce34a8
Joey <59043772+pan-zhao-yu@users.noreply.github.com>
2024-08-23 14:05:31 +1000

    Time: 20 ms (32.82%), Space: 45.5 MB (12.75%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.java
+++ 0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -1,26 +1,28 @@
 class Solution {
     public int countSubstrings(String s) {
-        if(s.length() < 2){
-            return s.length();
-        }
+        boolean[][] dp = new boolean[s.length()][s.length()];
         int result = 0;
-        for(int i = 0; i < s.length(); i++){
-            int left = i;
-            int right = i;
-            while(left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)){
-                result++;
-                left--;
-                right++;
-            }
-            
-            left = i;
-            right = i + 1;
-            while(left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)){
-                result++;
-                left--;
-                right++;
+        for(int i = s.length() - 1; i >= 0; i--){
+            for(int j = 0; j < s.length(); j++){
+                if(j - i >= 2){
+                    if(s.charAt(i) == s.charAt(j)){
+                        if(dp[i + 1][j - 1]){
+                            dp[i][j] = dp[i + 1][j - 1];
+                            result++;
+                        }
+                    }
+                }else if(j - i == 1 && s.charAt(i) == s.charAt(j)){
+                    dp[i][j] = true;
+                    result++;
+                }else if(j == i){
+                    dp[i][j] = true;
+                    result++;
+                }
+                
             }
         }
         return result;
     }
 }
+//if(s[i] == s[j]) 
+// if(dp[i + 1][j - 1]) dp[i][j] = dp[i + 1][j - 1];
