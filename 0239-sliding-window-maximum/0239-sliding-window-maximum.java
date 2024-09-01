09640ada52afee7399810e5201a0ccf01ce9adf6
Joey <59043772+pan-zhao-yu@users.noreply.github.com>
2024-09-01 16:55:48 +1000

    Time: 32 ms (48.07%), Space: 58.6 MB (64.82%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.java
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.java
@@ -1,25 +1,18 @@
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
-        ArrayDeque<Integer> deque = new ArrayDeque<>();
-        int n = nums.length;
-        int[] res = new int[n - k + 1];
-        int idx = 0;
-        for(int i = 0; i < n; i++) {
-            // 根据题意，i为nums下标，是要在[i - k + 1, i] 中选到最大值，只需要保证两点
-            // 1.队列头结点需要在[i - k + 1, i]范围内，不符合则要弹出
-            while(!deque.isEmpty() && deque.peek() < i - k + 1){
-                deque.poll();
+        int[] res = new int[nums.length - k + 1];
+        Deque<Integer> q = new ArrayDeque<>();
+        int index = 0;
+        for(int i = 0; i < nums.length; i++){
+            while(!q.isEmpty() && q.peek() < i - k + 1){
+                q.poll();
             }
-            // 2.既然是单调，就要保证每次放进去的数字要比末尾的都大，否则也弹出
-            while(!deque.isEmpty() && nums[deque.peekLast()] < nums[i]) {
-                deque.pollLast();
+            while(!q.isEmpty() && nums[q.peekLast()] < nums[i]){
+                q.pollLast();
             }
-
-            deque.offer(i);
-
-            // 因为单调，当i增长到符合第一个k范围的时候，每滑动一步都将队列头节点放入结果就行了
+            q.offerLast(i);
             if(i >= k - 1){
-                res[idx++] = nums[deque.peek()];
+                res[index++] = nums[q.peek()];
             }
         }
         return res;
