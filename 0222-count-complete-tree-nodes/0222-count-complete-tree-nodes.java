cc92d202837c976a40fc6be52e2bd011a5b7bb2b
Joey <59043772+pan-zhao-yu@users.noreply.github.com>
2024-09-03 22:27:52 +1000

    Time: 0 ms (100.00%), Space: 47.3 MB (78.89%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.java
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.java
@@ -14,8 +14,27 @@
  * }
  */
 class Solution {
+    /**
+     * 针对完全二叉树的解法
+     *
+     * 满二叉树的结点数为：2^depth - 1
+     */
     public int countNodes(TreeNode root) {
-        if(root == null) return 0;
-        return 1 + countNodes(root.left) + countNodes(root.right);
+        if (root == null) return 0;
+        TreeNode left = root.left;
+        TreeNode right = root.right;
+        int leftDepth = 0, rightDepth = 0; // 这里初始为0是有目的的，为了下面求指数方便
+        while (left != null) {  // 求左子树深度
+            left = left.left;
+            leftDepth++;
+        }
+        while (right != null) { // 求右子树深度
+            right = right.right;
+            rightDepth++;
+        }
+        if (leftDepth == rightDepth) {
+            return (2 << leftDepth) - 1; // 注意(2<<1) 相当于2^2，所以leftDepth初始为0
+        }
+        return countNodes(root.left) + countNodes(root.right) + 1;
     }
 }
