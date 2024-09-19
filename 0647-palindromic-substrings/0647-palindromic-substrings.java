8b2a165a60c330f7660df0b1ce83354f12358203
Joey <59043772+pan-zhao-yu@users.noreply.github.com>
2024-09-19 13:18:56 +1000

    Time: 16 ms (43.48%), Space: 45.5 MB (13.04%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.java
+++ 0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -1,28 +1,16 @@
 class Solution {
     public int countSubstrings(String s) {
         boolean[][] dp = new boolean[s.length()][s.length()];
-        int result = 0;
-        for(int i = s.length() - 1; i >= 0; i--){
-            for(int j = 0; j < s.length(); j++){
-                if(j - i >= 2){
-                    if(s.charAt(i) == s.charAt(j)){
-                        if(dp[i + 1][j - 1]){
-                            dp[i][j] = dp[i + 1][j - 1];
-                            result++;
-                        }
-                    }
-                }else if(j - i == 1 && s.charAt(i) == s.charAt(j)){
+        
+        int res = 0;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            for (int j = i; j < s.length(); j++) {
+                if (s.charAt(i) == s.charAt(j) && (j - i <= 1 || dp[i + 1][j - 1])) {
+                    res++;
                     dp[i][j] = true;
-                    result++;
-                }else if(j == i){
-                    dp[i][j] = true;
-                    result++;
                 }
-                
             }
         }
-        return result;
+        return res;
     }
 }
-//if(s[i] == s[j]) 
-// if(dp[i + 1][j - 1]) dp[i][j] = dp[i + 1][j - 1];
