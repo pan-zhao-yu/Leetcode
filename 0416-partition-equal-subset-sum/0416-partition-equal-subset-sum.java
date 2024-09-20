3cde3a0a8964b70625640a2dbae0629695a24549
Joey <59043772+pan-zhao-yu@users.noreply.github.com>
2024-09-20 21:35:22 +1000

    Time: 29 ms (78.03%), Space: 57.4 MB (21.33%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -6,15 +6,21 @@ class Solution {
         }
         if(sum % 2 != 0) return false;
         int target = sum / 2;
-        int[] dp = new int[target + 1];
-        for(int n : nums){
-            for(int j = target; j >= n; j--){
-                dp[j] = Math.max(dp[j], dp[j - n] + n);
+        int[][] dp = new int[nums.length + 1][target + 1];
+        for(int i = 1; i <= nums.length; i++){
+            for(int j = 1; j <= target; j++){
+                if(j >= nums[i - 1]){
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - nums[i - 1]] + nums[i - 1]);
+                }else{
+                    dp[i][j] = dp[i - 1][j];
+                }
             }
         }
-        return dp[target] == target;
+        return dp[nums.length][target] == target;
     }
 }
+//dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - nums[i - 1]] + nums[i - 1]);
+
 
 
 // class Solution {
@@ -25,19 +31,14 @@ class Solution {
 //         }
 //         if(sum % 2 != 0) return false;
 //         int target = sum / 2;
-//         int[][] dp = new int[nums.length + 1][target + 1];
-
-//         for(int i = 1; i < nums.length + 1; i++){
-//             for(int j = 1; j < target + 1; j++){
-//                 if (j >= nums[i - 1]) {
-//                     dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - nums[i - 1]] + nums[i - 1]);
-//                 } else {
-//                     dp[i][j] = dp[i - 1][j];
-//                 }
+//         int[] dp = new int[target + 1];
+//         for(int n : nums){
+//             for(int j = target; j >= n; j--){
+//                 dp[j] = Math.max(dp[j], dp[j - n] + n);
 //             }
 //         }
-//         return dp[nums.length][target] == target;
+//         return dp[target] == target;
 //     }
 // }
-//dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - nums[i - 1]] + nums[i - 1])
-//dp[j] = Math.max(dp[j], dp[j - nums[i - 1] + nums[i - 1]])
+
+
