3fe41ab535947d478f5abe35ac86bff439b2808f
Joey <59043772+pan-zhao-yu@users.noreply.github.com>
2024-09-24 03:29:16 +1000

    Time: 0 ms (100.00%), Space: 40.3 MB (37.06%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.java
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.java
@@ -1,32 +1,15 @@
 class Solution {
-    
     public int numTrees(int n) {
-        int x = 0;
-        if(n == 0){
-            return 1;
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for(int i = 2; i <= n; i++){
+            for(int j = 1; j <= i; j++){
+                dp[i] += dp[j - 1] * dp[i - j];
+            }
         }
-        if(n == 1) return 1;
-        
-        for(int i = 0; i <= n - 1; i++){
-            x += numTrees(i) * numTrees(n - 1 - i);
-        }
-        return x;
+        return dp[n];
     }
 }
-
-
-
-// class Solution {
-//     public int numTrees(int n) {
-//         int[] dp = new int[n + 1];
-//         dp[0] = 1;
-//         dp[1] = 1;
-//         for(int i = 2; i <= n; i++){
-//             for(int j = 1; j <= i; j++){
-//                 dp[i] += dp[j - 1] * dp[i - j];
-//             }
-//         }
-//         return dp[n];
-//     }
-// }
-//dp[i] = dp[j - 1] * dp[i - j];
+//dp[i] means how many unique ways that the tree head is from 1 to i
+//dp[i] += dp[i - 1] * dp[i - j];
