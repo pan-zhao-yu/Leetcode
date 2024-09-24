1226823350ef291bc9e8926cf30e689d7ac8e7a8
Joey <59043772+pan-zhao-yu@users.noreply.github.com>
2024-09-24 15:23:14 +1000

    Time: 29 ms (77.87%), Space: 55.8 MB (40.37%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -7,6 +7,9 @@ class Solution {
         if(sum % 2 != 0) return false;
         int target = sum / 2;
         int[][] dp = new int[nums.length + 1][target + 1];
+        for(int i = 0; i < nums.length; i++){
+            dp[i][0] = 0;
+        }
         for(int i = 1; i <= nums.length; i++){
             for(int j = 1; j <= target; j++){
                 if(j >= nums[i - 1]){
@@ -19,26 +22,6 @@ class Solution {
         return dp[nums.length][target] == target;
     }
 }
-//dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - nums[i - 1]] + nums[i - 1]);
 
-
-
-// class Solution {
-//     public boolean canPartition(int[] nums) {
-//         int sum = 0;
-//         for(int n : nums){
-//             sum += n;
-//         }
-//         if(sum % 2 != 0) return false;
-//         int target = sum / 2;
-//         int[] dp = new int[target + 1];
-//         for(int n : nums){
-//             for(int j = target; j >= n; j--){
-//                 dp[j] = Math.max(dp[j], dp[j - n] + n);
-//             }
-//         }
-//         return dp[target] == target;
-//     }
-// }
-
-
+//dp[i][j] = the max sum we can adds up by using the first i th item, in target j
+//dp[i][j] = max(dp[i -1][j], dp[i - 1][j - nums[i - 1]] + nums[i - 1]);
