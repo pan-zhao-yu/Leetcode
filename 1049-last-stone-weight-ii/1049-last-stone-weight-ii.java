1a7b3954d5866a032880d072de057316c293b653
Joey <59043772+pan-zhao-yu@users.noreply.github.com>
2024-09-24 16:27:38 +1000

    Time: 5 ms (37.78%), Space: 41.4 MB (57.11%) - LeetHub

--- 1049-last-stone-weight-ii/1049-last-stone-weight-ii.java
+++ 1049-last-stone-weight-ii/1049-last-stone-weight-ii.java
@@ -5,12 +5,17 @@ class Solution {
             sum += n;
         }
         int target = sum / 2;
-        int[] dp = new int[target + 1];
-        for(int stone : stones){
-            for(int j = target; j >= stone; j--){
-                dp[j] = Math.max(dp[j], dp[j - stone] + stone);
+        int[][] dp = new int[stones.length + 1][target + 1];
+        for(int i = 1; i <= stones.length; i++){
+            for(int j = 1; j <= target; j++){
+                if(j >= stones[i - 1]){
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - stones[i - 1]] + stones[i - 1]);
+                }else{
+                    dp[i][j] = dp[i - 1][j];
+                }
             }
         }
-        return sum - 2 * dp[target];
+        return sum - 2 * dp[stones.length][target];
     }
 }
+
