0af60eb08a3329bf576797f0ea4fecc46a869a3c
Joey <59043772+pan-zhao-yu@users.noreply.github.com>
2024-09-24 19:37:28 +1000

    Time: 5 ms (87.05%), Space: 43.6 MB (42.28%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -4,23 +4,27 @@ class Solution {
         for(int n : nums){
             sum += n;
         }
-        if(sum < Math.abs(target) || (sum + target) % 2 != 0) return 0;
+        if ((sum + target) % 2 != 0 || sum < Math.abs(target)) {
+            return 0;
+        }
         int left = (sum + target) / 2;
         int[][] dp = new int[nums.length + 1][left + 1];
         for(int i = 0; i <= nums.length; i++){
             dp[i][0] = 1;
         }
         for(int i = 1; i <= nums.length; i++){
-            for(int j = 0; j <= left; j++){
+            for(int j = 0; j <= left;j++){
                 if(j >= nums[i - 1]){
                     dp[i][j] = dp[i - 1][j] + dp[i - 1][j - nums[i - 1]];
                 }else{
                     dp[i][j] = dp[i - 1][j];
                 }
-                
             }
         }
         return dp[nums.length][left];
     }
 }
-//dp[i][j] = dp[i - 1][j] + dp[i - 1][j - nums[i]];
+
+//dp[i][j] = dp[i - 1][j] + dp[i - 1][j - nums[i - 1]];
+//j = left;
+//left = (sum + target) / 2
