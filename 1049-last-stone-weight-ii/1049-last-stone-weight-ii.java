c9dc0fe38213054ef483c263b82ecbfb505ec3a0
Joey <59043772+pan-zhao-yu@users.noreply.github.com>
2024-09-26 20:11:52 +1000

    Time: 6 ms (25.24%), Space: 42.3 MB (25.49%) - LeetHub

--- 1049-last-stone-weight-ii/1049-last-stone-weight-ii.java
+++ 1049-last-stone-weight-ii/1049-last-stone-weight-ii.java
@@ -1,13 +1,16 @@
 class Solution {
     public int lastStoneWeightII(int[] stones) {
         int sum = 0;
-        for(int n : stones){
-            sum += n;
+        for(int stone : stones){
+            sum += stone;
         }
         int target = sum / 2;
         int[][] dp = new int[stones.length + 1][target + 1];
+        for(int i = 0; i <= stones.length; i++){
+            dp[i][0] = 0;
+        }
         for(int i = 1; i <= stones.length; i++){
-            for(int j = 1; j <= target; j++){
+            for(int j = 0; j <= target; j++){
                 if(j >= stones[i - 1]){
                     dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - stones[i - 1]] + stones[i - 1]);
                 }else{
@@ -18,4 +21,5 @@ class Solution {
         return sum - 2 * dp[stones.length][target];
     }
 }
-
+//dp[i][j] means the max sum we can achive by using the first i th element, in range j
+//dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - stones[i - 1]] + stones[i - 1]);
