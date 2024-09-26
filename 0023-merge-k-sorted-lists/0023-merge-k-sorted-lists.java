b3bb78944285c0c2faad32bf6743692d46d898f0
Joey <59043772+pan-zhao-yu@users.noreply.github.com>
2024-09-26 17:23:13 +1000

    Time: 1 ms (100.00%), Space: 44.6 MB (30.05%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -10,21 +10,56 @@
  */
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
-        PriorityQueue<ListNode> pq = new PriorityQueue<>((a, b) -> a.val - b.val);
-        for(ListNode temp : lists){
-            if(temp != null){
-                pq.offer(temp);
-            }
-        }
+        if(lists == null || lists.length == 0) return null;
+        return merge(lists, 0, lists.length - 1);
+    }
+    private ListNode merge(ListNode[] lists, int left, int right){
+        if(left == right) return lists[left];
+        int mid = left + (right - left) / 2;
+        ListNode l1 = merge(lists, left, mid);
+        ListNode l2 = merge(lists, mid + 1, right);
+        return mergeList(l1, l2);
+    }
+    private ListNode mergeList(ListNode l1, ListNode l2){
         ListNode dummy = new ListNode();
         ListNode curr = dummy;
-        while(!pq.isEmpty()){
-            curr.next = pq.poll();
-            curr = curr.next;
-            if(curr.next != null){
-                pq.offer(curr.next);
+        while(l1 != null && l2 != null){
+            if(l1.val < l2.val){
+                curr.next = l1;
+                curr = curr.next;
+                l1 = l1.next;
+            }else{
+                curr.next = l2;
+                curr = curr.next;
+                l2 = l2.next;
             }
+        }
+        if(l1 != null){
+            curr.next = l1;
+        }else if(l2 != null){
+            curr.next = l2;
         }
         return dummy.next;
     }
 }
+
+// class Solution {
+//     public ListNode mergeKLists(ListNode[] lists) {
+//         PriorityQueue<ListNode> pq = new PriorityQueue<>((a, b) -> a.val - b.val);
+//         for(ListNode temp : lists){
+//             if(temp != null){
+//                 pq.offer(temp);
+//             }
+//         }
+//         ListNode dummy = new ListNode();
+//         ListNode curr = dummy;
+//         while(!pq.isEmpty()){
+//             curr.next = pq.poll();
+//             curr = curr.next;
+//             if(curr.next != null){
+//                 pq.offer(curr.next);
+//             }
+//         }
+//         return dummy.next;
+//     }
+// }
