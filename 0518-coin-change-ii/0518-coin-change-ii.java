c3b6978afa4074b3a26b86aed515dbb6418ec6ef
Joey <59043772+pan-zhao-yu@users.noreply.github.com>
2024-09-27 16:05:33 +1000

    Time: 10 ms (25.18%), Space: 49.2 MB (6.45%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -1,15 +1,37 @@
 class Solution {
     public int change(int amount, int[] coins) {
-        int[] dp = new int[amount + 1];
-        dp[0] = 1;
-        for(int coin : coins){
-            for(int j = coin; j <= amount; j++){
-                dp[j] = dp[j] + dp[j - coin];
+        int[][] dp = new int[coins.length + 1][amount + 1];
+        for(int i = 0; i <= coins.length; i++){
+             dp[i][0] = 1;
+         }
+        for(int i = 1; i <= coins.length; i++){
+            for(int j = 1; j <= amount; j++){
+                if(j >= coins[i - 1]){
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - coins[i - 1]];
+                }else{
+                    dp[i][j] = dp[i - 1][j];
+                }
             }
         }
-        return dp[amount];
+        return dp[coins.length][amount];
     }
 }
+//dp[i][j] means by using 0 to i th coins denominations, how may ways we can make up amount j;
+//dp[i][j] = dp[i - 1][j] + dp[i - 1][j - coins[i - 1]];
+
+
+// class Solution {
+//     public int change(int amount, int[] coins) {
+//         int[] dp = new int[amount + 1];
+//         dp[0] = 1;
+//         for(int coin : coins){
+//             for(int j = coin; j <= amount; j++){
+//                 dp[j] = dp[j] + dp[j - coin];
+//             }
+//         }
+//         return dp[amount];
+//     }
+// }
 
 
 // class Solution {
@@ -25,9 +47,11 @@ class Solution {
 //                 }else{
 //                     dp[i][j] = dp[i - 1][j];
 //                 }
+//                 System.out.print(dp[i][j] + " ");
 //             }
+//             System.out.println(" ");
 //         }
 //         return dp[coins.length][amount];
 //     }
 // }
-//dp[i][j] = dp[i - 1][j] + dp[i][j - coins[i]]
+// //dp[i][j] = dp[i - 1][j] + dp[i][j - coins[i]]
