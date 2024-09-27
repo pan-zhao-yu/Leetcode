e6b8a03bb1da6b553d00b2cf74fd2d3b733a81dc
Joey <59043772+pan-zhao-yu@users.noreply.github.com>
2024-09-27 16:11:07 +1000

    Time: 4 ms (79.51%), Space: 41.4 MB (76.95%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -16,20 +16,6 @@ class Solution {
 
 // class Solution {
 //     public int change(int amount, int[] coins) {
-//         int[] dp = new int[amount + 1];
-//         dp[0] = 1;
-//         for(int coin : coins){
-//             for(int j = coin; j <= amount; j++){
-//                 dp[j] = dp[j] + dp[j - coin];
-//             }
-//         }
-//         return dp[amount];
-//     }
-// }
-
-
-// class Solution {
-//     public int change(int amount, int[] coins) {
 //         int[][] dp = new int[coins.length + 1][amount + 1];
 //         for(int i = 0; i <= coins.length; i++){
 //             dp[i][0] = 1;
