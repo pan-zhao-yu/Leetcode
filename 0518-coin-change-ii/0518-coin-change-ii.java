9b3a95a229524393042bfc020670d6923e398516
Joey <59043772+pan-zhao-yu@users.noreply.github.com>
2024-09-27 16:10:29 +1000

    Time: 3 ms (89.29%), Space: 40.8 MB (93.08%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -1,23 +1,17 @@
 class Solution {
     public int change(int amount, int[] coins) {
-        int[][] dp = new int[coins.length + 1][amount + 1];
-        for(int i = 0; i <= coins.length; i++){
-             dp[i][0] = 1;
-         }
-        for(int i = 1; i <= coins.length; i++){
-            for(int j = 1; j <= amount; j++){
-                if(j >= coins[i - 1]){
-                    dp[i][j] = dp[i - 1][j] + dp[i][j - coins[i - 1]];
-                }else{
-                    dp[i][j] = dp[i - 1][j];
-                }
+        int[] dp = new int[amount + 1];
+        dp[0] = 1;
+        for(int coin : coins){
+            for(int j = coin; j <= amount; j++){
+                dp[j] += dp[j - coin];
             }
         }
-        return dp[coins.length][amount];
+        return dp[amount];
     }
 }
-//dp[i][j] means by using 0 to i th coins denominations, how may ways we can make up amount j;
-//dp[i][j] = dp[i - 1][j] + dp[i - 1][j - coins[i - 1]];
+//dp[j] means how many ways we can make up to amount j
+//dp[j] = dp[j] + dp[j - coins];
 
 
 // class Solution {
