7f48ea6f65f33d5f26032f80eecc5bafde490b3d
Joey <59043772+pan-zhao-yu@users.noreply.github.com>
2024-09-29 00:24:18 +1000

    Time: 4 ms (79.68%), Space: 41.6 MB (73.08%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -4,34 +4,10 @@ class Solution {
         dp[0] = 1;
         for(int coin : coins){
             for(int j = coin; j <= amount; j++){
-                dp[j] += dp[j - coin];
+                dp[j] = dp[j] + dp[j - coin];
             }
         }
         return dp[amount];
     }
 }
-//dp[j] means how many ways we can make up to amount j
-//dp[j] = dp[j] + dp[j - coins];
-
-
-// class Solution {
-//     public int change(int amount, int[] coins) {
-//         int[][] dp = new int[coins.length + 1][amount + 1];
-//         for(int i = 0; i <= coins.length; i++){
-//             dp[i][0] = 1;
-//         }
-//         for(int i = 1; i <= coins.length; i++){
-//             for(int j = 1; j <= amount; j++){
-//                 if(j >= coins[i - 1]){
-//                     dp[i][j] = dp[i - 1][j] + dp[i][j - coins[i - 1]];
-//                 }else{
-//                     dp[i][j] = dp[i - 1][j];
-//                 }
-//                 System.out.print(dp[i][j] + " ");
-//             }
-//             System.out.println(" ");
-//         }
-//         return dp[coins.length][amount];
-//     }
-// }
-// //dp[i][j] = dp[i - 1][j] + dp[i][j - coins[i]]
+//dp[i][j] = dp[i - 1][j] + dp[i][j - coins]
