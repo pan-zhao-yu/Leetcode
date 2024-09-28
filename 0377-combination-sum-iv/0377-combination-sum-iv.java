cf31ee9e34a1785093eae2408d3558190701fba7
Joey <59043772+pan-zhao-yu@users.noreply.github.com>
2024-09-29 03:57:51 +1000

    Time: 1 ms (70.34%), Space: 41 MB (45.82%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.java
+++ 0377-combination-sum-iv/0377-combination-sum-iv.java
@@ -1,30 +1,17 @@
 class Solution {
     public int combinationSum4(int[] nums, int target) {
-        int n = nums.length;
-        int[][] dp = new int[target + 1][n + 1];
-        
-        // Initialize dp[0][j] = 1 for all j because there's one way to get sum 0 (by choosing nothing)
-        for (int j = 0; j <= n; j++) {
-            dp[0][j] = 1;
-        }
-        
-        // Fill the dp array
-        for (int i = 1; i <= target; i++) {
-            for (int j = 1; j <= n; j++) {
-                // Number of ways to form sum i using the first j elements without including nums[j-1]
-                dp[i][j] = dp[i][j-1];
-                
-                // Include the current element nums[j-1] if it fits into the sum
-                if (i >= nums[j-1]) {
-                    dp[i][j] += dp[i - nums[j-1]][n]; // Use all elements to form remaining sum
+        int[] dp = new int[target + 1];
+        dp[0] = 1;
+        for (int i = 0; i <= target; i++) {
+            for (int j = 0; j < nums.length; j++) {
+                if (i >= nums[j]) {
+                    dp[i] += dp[i - nums[j]];
                 }
             }
         }
-        
-        return dp[target][n];
+        return dp[target];
     }
 }
 
-
-
-
+//dp[i] means the number of possible way to add up by using the first i th element
+//dp[i] = dp[i] + dp[i - 1];
