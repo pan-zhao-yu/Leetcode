0ca5b18c2b3d14fa1bebc7323449cfa99e9c2245
Joey <59043772+pan-zhao-yu@users.noreply.github.com>
2024-09-29 04:08:07 +1000

    Time: 3 ms (10.82%), Space: 41.5 MB (11.52%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.java
+++ 0377-combination-sum-iv/0377-combination-sum-iv.java
@@ -1,17 +1,29 @@
 class Solution {
     public int combinationSum4(int[] nums, int target) {
-        int[] dp = new int[target + 1];
-        dp[0] = 1;
-        for (int i = 0; i <= target; i++) {
-            for (int j = 0; j < nums.length; j++) {
-                if (i >= nums[j]) {
-                    dp[i] += dp[i - nums[j]];
+        // Create a dp array of size (target + 1)
+        int[][] dp = new int[target + 1][nums.length + 1];
+        
+        // Base case: There's one way to make the amount 0
+        for (int j = 0; j <= nums.length; j++) {
+            dp[0][j] = 1;
+        }
+
+        // Fill the DP table
+        for (int i = 1; i <= target; i++) {
+            for (int j = 1; j <= nums.length; j++) {
+                // Include the current number nums[j-1] if possible
+                if (i >= nums[j - 1]) {
+                    // Here, we are using dp[i][j] += dp[i - nums[j-1]][nums.length]
+                    dp[i][j] = dp[i][j - 1] + dp[i - nums[j - 1]][nums.length];
+                } else {
+                    // Exclude the current number if it's larger than the remaining target
+                    dp[i][j] = dp[i][j - 1];
                 }
             }
         }
-        return dp[target];
+
+        // The answer is the number of ways to form target using all numbers in nums
+        return dp[target][nums.length];
     }
 }
 
-//dp[i] means the number of possible way to add up by using the first i th element
-//dp[i] = dp[i] + dp[i - 1];
