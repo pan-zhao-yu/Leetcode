b6a5dc4a8ebc01b556a14a05fa2c682e25b96215
Joey <59043772+pan-zhao-yu@users.noreply.github.com>
2024-09-30 20:20:12 +1000

    Time: 34 ms (67.17%), Space: 41.9 MB (94.38%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.java
+++ 0279-perfect-squares/0279-perfect-squares.java
@@ -3,7 +3,7 @@ class Solution {
         int[] dp = new int[n + 1];
         Arrays.fill(dp, Integer.MAX_VALUE);
         dp[0] = 0;
-        for(int i = 1; i <= n; i++){
+        for(int i = 0; i <= n; i++){
             for(int j = 1; j * j <= i; j++){
                 dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
             }
@@ -11,30 +11,5 @@ class Solution {
         return dp[n];
     }
 }
-//dp[j] = Math.min(dp[j], dp[j - square] + 1);
 
-// class Solution {
-//     public int numSquares(int n) {
-//         List<Integer> squares = new LinkedList<>();
-//         for(int i = 1; i * i <= n; i++){
-//             squares.add(i * i);
-//         }
-//         int[][] dp = new int[squares.size() + 1][n + 1];
-//         for(int i = 0; i <= squares.size(); i++){
-//             dp[i][0] = 0;
-//         }
-//         for(int j = 1; j <= n; j++){
-//             dp[0][j] = Integer.MAX_VALUE - 1;
-//         }
-//         for(int i = 1; i <= squares.size(); i++){
-//             for(int j = 1; j <= n; j++){
-//                 if(j >= squares.get(i - 1)){
-//                     dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - squares.get(i - 1)] + 1);
-//                 }else{
-//                     dp[i][j] = dp[i -1][j];
-//                 }
-//             }
-//         }
-//         return dp[squares.size()][n];
-//     }
-// }
+//dp[i] means the min number of ways to sum to i
