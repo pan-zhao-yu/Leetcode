16cc1d0888e3cd942e29018829f901faef112220
Joey <59043772+pan-zhao-yu@users.noreply.github.com>
2024-10-02 21:12:52 +1000

    Time: 0 ms (100.00%), Space: 41.3 MB (9.95%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,12 +1,13 @@
 class Solution {
     public int rob(int[] nums) {
-        int dp1 = 0;
-        int dp2 = 0;
-        for(int n : nums){
-            int temp = Math.max(dp1 + n, dp2);
-            dp1 = dp2;
-            dp2 = temp;
+        int[] dp = new int[nums.length + 1];
+        dp[0] = 0;
+        dp[1] = nums[0];
+        for(int i = 2; i <= nums.length; i++){
+            dp[i] = Math.max(dp[i - 2] + nums[i - 1], dp[i - 1]);
         }
-        return dp2;
+        return dp[nums.length];
     }
 }
+// dp[i] means the max profit we can get for robbing from 0 to i house(with in the rules);
+// dp[i] = max(dp[i - 2] + num[i], dp[i - 1]);
