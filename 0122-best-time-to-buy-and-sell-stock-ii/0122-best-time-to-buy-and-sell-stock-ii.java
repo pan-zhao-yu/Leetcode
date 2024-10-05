5279c7fc84f0badd6b57922a6c809954fea7ab42
Joey <59043772+pan-zhao-yu@users.noreply.github.com>
2024-10-05 15:25:58 +1000

    Time: 4 ms (6.52%), Space: 46.2 MB (9.52%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,15 +1,16 @@
 class Solution {
     public int maxProfit(int[] prices) {
         int[][] dp = new int[prices.length][2];
-        dp[0][0] = -prices[0];
-        dp[0][1] = 0;
+        dp[0][0] = 0;
+        dp[0][1] = -prices[0];
         for(int i = 1; i < prices.length; i++){
-            dp[i][0] = Math.max(dp[i - 1][1] - prices[i], dp[i - 1][0]);
-            dp[i][1] = Math.max(dp[i - 1][0] + prices[i], dp[i - 1][1]);
+            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
+            dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] - prices[i]);
         }
-        return dp[prices.length - 1][1];
+        return dp[prices.length - 1][0];
     }
 }
-
-//(hold)dp[i][0] == Math.max(dp[i - 1][0], dp[i - 1][1] - prices[i]);
-//(sell)dp[i][1] == Math.max(dp[i - 1][1], dp[i][0] - prices[i]);
+//dp[i][0] means the max money possible on hand without holding stock on i th day
+//dp[i][1] max on hand with holding stock, i th day
+//dp[i][0] = max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
+//dp[i][1] = max(dp[i - 1][1], -prices[i]);
