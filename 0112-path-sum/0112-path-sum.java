bbf1bc8f87f27d64b8404fe8837667f0607cdfd4
Joey <59043772+pan-zhao-yu@users.noreply.github.com>
2024-10-10 21:03:47 +1100

    Time: 0 ms (100.00%), Space: 43.1 MB (49.66%) - LeetHub

--- 0112-path-sum/0112-path-sum.java
+++ 0112-path-sum/0112-path-sum.java
@@ -22,13 +22,8 @@ class Solution {
         if(target == node.val && node.left == null && node.right == null){
             return true;
         }
-        if(node.left == null && node.right == null) return false;
-        if(node.left != null){
-            if(pathSum(node.left, target - node.val)) return true;
-        }
-        if(node.right != null){
-            if(pathSum(node.right, target - node.val)) return true;
-        }
+        if(node.left != null && pathSum(node.left, target - node.val)) return true;
+        if(node.right != null && pathSum(node.right, target - node.val)) return true;
         return false;
     }
 }
