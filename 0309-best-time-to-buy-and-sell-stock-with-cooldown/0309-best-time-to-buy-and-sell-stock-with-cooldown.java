1caa5a1d0b2f7b911c096b7786238b8c3dc93d61
Joey <59043772+pan-zhao-yu@users.noreply.github.com>
2024-10-13 22:29:36 +1100

    Time: 1 ms (84.86%), Space: 42.1 MB (21.43%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.java
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -5,8 +5,9 @@ class Solution {
         dp[0][1] = 0;
         dp[0][2] = 0;
         for(int i = 1; i < prices.length; i++){
-            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][2] - prices[i]);
             dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] + prices[i]);
+            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][2] - prices[i]);
+            
             dp[i][2] = Math.max(dp[i - 1][2], dp[i - 1][1]);
         }
         return dp[prices.length -1][1];
