96799fa66517dba2c6e7922225171ef0f233d146
Joey <59043772+pan-zhao-yu@users.noreply.github.com>
2024-10-15 20:49:07 +1100

    Time: 21 ms (77.65%), Space: 54.6 MB (66.14%) - LeetHub

--- 0718-maximum-length-of-repeated-subarray/0718-maximum-length-of-repeated-subarray.java
+++ 0718-maximum-length-of-repeated-subarray/0718-maximum-length-of-repeated-subarray.java
@@ -1,17 +1,18 @@
 class Solution {
     public int findLength(int[] nums1, int[] nums2) {
-        int dp[][] = new int[nums1.length + 1][nums2.length + 1];
-        int result = 0;
+        int[][] dp = new int[nums1.length + 1][nums2.length + 1];
+        
+        int maxLen = 0;
         for(int i = 1; i <= nums1.length; i++){
             for(int j = 1; j <= nums2.length; j++){
                 if(nums1[i - 1] == nums2[j - 1]){
                     dp[i][j] = dp[i - 1][j - 1] + 1;
-                    result = Math.max(result, dp[i][j]);
+                    maxLen = Math.max(dp[i][j], maxLen);
                 }
             }
         }
-        return result;
+        return maxLen;
     }
 }
-
-//if(nums[i] == nums[j]) dp[i][j] = dp[i - 1][j - 1] + 1;
+//dp[i][j] = at i th element from nums1, at j th element from nums2, max repeated subarray len;
+//if(nums1[i] == nums2[j]) dp[i][j] = dp[i - 1][j - 1] + 1
