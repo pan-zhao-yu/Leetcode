4e707e04b2254d36aeda6be90956a9271b94d2cf
Joey <59043772+pan-zhao-yu@users.noreply.github.com>
2024-10-21 02:01:50 +1100

    Time: 5 ms (65.33%), Space: 45 MB (49.55%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -1,12 +1,17 @@
 class Solution {
     public int minDistance(String word1, String word2) {
+        if(word1.length() == 0 && word2.length() == 0) return 0;
+        if(word1.length() == 0) return word2.length();
+        if(word2.length() == 0) return word1.length();
         int[][] dp = new int[word1.length() + 1][word2.length() + 1];
+        
         for (int i = 0; i <= word1.length(); i++) {
-            dp[i][0] = i; // word1 转换为空字符串的操作次数
+            dp[i][0] = i;  // Deleting all characters from word1 to match an empty word2
         }
         for (int j = 0; j <= word2.length(); j++) {
-            dp[0][j] = j; // 空字符串转换为 word2 的操作次数
+            dp[0][j] = j;  // Inserting all characters from word2 to match an empty word1
         }
+        
         for(int i = 1; i <= word1.length(); i++){
             for(int j = 1; j <= word2.length(); j++){
                 if(word1.charAt(i - 1) == word2.charAt(j - 1)){
