68aea5a8639d6553178909287e626862b95f8e6d
Joey <59043772+pan-zhao-yu@users.noreply.github.com>
2024-10-21 21:08:26 +1100

    Time: 14 ms (47.72%), Space: 45.3 MB (24.92%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.java
+++ 0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -3,9 +3,9 @@ class Solution {
         boolean[][] dp = new boolean[s.length()][s.length()];
         
         int res = 0;
-        for (int i = s.length() - 1; i >= 0; i--) {
-            for (int j = i; j < s.length(); j++) {
-                if (s.charAt(i) == s.charAt(j) && (j - i <= 1 || dp[i + 1][j - 1])) {
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = i; j >= 0; j--) {
+                if (s.charAt(i) == s.charAt(j) && (i - j <= 1 || dp[i - 1][j + 1])) {
                     res++;
                     dp[i][j] = true;
                 }
