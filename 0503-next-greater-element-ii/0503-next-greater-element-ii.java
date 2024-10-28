a866cf747b825d2d523d0af4ccde907ff5318755
Joey <59043772+pan-zhao-yu@users.noreply.github.com>
2024-10-28 23:18:25 +1100

    Time: 9 ms (91.42%), Space: 46.2 MB (36.01%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.java
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.java
@@ -1,26 +1,18 @@
 class Solution {
     public int[] nextGreaterElements(int[] nums) {
-        int[] nums2 = new int[nums.length * 2];
-        for(int i = 0; i < nums.length; i++){
-            nums2[i] = nums[i];
-            nums2[i + nums.length] = nums[i];
-        }
-        
-        Stack<Integer> stack = new Stack();
-        int[] result = new int[nums2.length];
+        Stack<Integer> stack = new Stack<>();
+        int[] result = new int[nums.length];
         Arrays.fill(result, -1);
-        for(int i = 0; i < nums2.length; i++){
-            while(!stack.isEmpty() && nums2[i] > nums2[stack.peek()]){
-                result[stack.peek()] = nums2[i];
-                stack.pop();
-            }
-            stack.push(i);
-        }
 
-        int[] realResult = new int[nums.length];
-        for(int i = 0; i < nums.length; i++){
-            realResult[i] = result[i];
+        for (int i = 0; i < nums.length * 2; i++) {
+            int currentIndex = i % nums.length;
+            while (!stack.isEmpty() && nums[currentIndex] > nums[stack.peek()]) {
+                result[stack.pop()] = nums[currentIndex];
+            }
+            if (i < nums.length) {
+                stack.push(currentIndex);
+            }
         }
-        return realResult;
+        return result;
     }
 }
