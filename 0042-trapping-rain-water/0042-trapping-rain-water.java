a5a265f29bb24d97842db446f556dc560b47c370
Joey <59043772+pan-zhao-yu@users.noreply.github.com>
2024-10-30 13:49:48 +1100

    Time: 1 ms (66.64%), Space: 46.1 MB (57.08%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,47 +1,24 @@
 class Solution {
     public int trap(int[] height) {
-        int traped = 0;
-        int leftM = 0;
-        int rightM = 0;
-        int[] leftMax = new int[height.length];
-        int[] rightMax = new int[height.length];
-        for(int i = 0; i < leftMax.length; i++){
-            leftMax[i] = leftM;
-            leftM = Math.max(height[i], leftM);
-        }
-        for(int i = leftMax.length - 1; i >=0; i--){
-            rightMax[i] = rightM;
-            rightM = Math.max(height[i], rightM);
-        }
+        int leftMax = 0;
+        int rightMax = 0;
+        int[] left = new int[height.length];
+        int[] right = new int[height.length];
+        int waterTrapped = 0;
+        
         for(int i = 0; i < height.length; i++){
-            int t = Math.min(leftMax[i], rightMax[i]) - height[i];
-            if(t > 0){
-                traped += t;
-            }
+            leftMax = Math.max(leftMax, height[i]);
+            left[i] = leftMax;
         }
-        return traped;
+        for(int i = height.length - 1; i >= 0; i--){
+            rightMax = Math.max(rightMax, height[i]);
+            right[i] = rightMax;
+        }
+        
+        for(int i = 0; i < height.length; i++){
+            int border = Math.min(left[i], right[i]);
+            waterTrapped += Math.max(0, border - height[i]);
+        }
+        return waterTrapped;
     }
 }
-
-// class Solution {
-//     public int trap(int[] height) {
-//         int left = 0;
-//         int right = height.length - 1;;
-//         int maxLeft = 0;
-//         int maxRight = 0;
-//         int trappedWater = 0;
-        
-//         while(left < right){
-//             if(height[left] < height[right]){
-//                 maxLeft = Math.max(height[left], maxLeft);
-//                 trappedWater += maxLeft - height[left];
-//                 left++;
-//             }else{
-//                 maxRight = Math.max(height[right], maxRight);
-//                 trappedWater += maxRight - height[right];
-//                 right--;
-//             }
-//         }
-//         return trappedWater;
-//     }
-// }
