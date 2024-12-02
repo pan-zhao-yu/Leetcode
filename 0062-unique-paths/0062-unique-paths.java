6367f2bd4a266b6fe0157b8a26f2436561ed98d6
Joey <59043772+pan-zhao-yu@users.noreply.github.com>
2024-12-02 21:06:17 +0800

    Time: 0 ms (100.00%), Space: 40.5 MB (40.04%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -4,8 +4,8 @@ class Solution {
         for(int i = 0; i < m; i++){
             dp[i][0] = 1;
         }
-        for(int j = 0; j < n; j++){
-            dp[0][j] = 1;
+        for(int i = 0; i < n; i++){
+            dp[0][i] = 1;
         }
         for(int i = 1; i < m; i++){
             for(int j = 1; j < n; j++){
@@ -15,5 +15,3 @@ class Solution {
         return dp[m - 1][n - 1];
     }
 }
-//dp[i][j] means how many unique paths to reach i j
-//dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
