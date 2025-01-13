21879ca3e8ca7bc36bb789212576b40c667d487b
Joey <59043772+pan-zhao-yu@users.noreply.github.com>
2025-01-13 21:43:18 +0800

    Time: 2 ms (99.94%), Space: 58.6 MB (38.82%) - LeetHub

--- 2270-number-of-ways-to-split-array/2270-number-of-ways-to-split-array.java
+++ 2270-number-of-ways-to-split-array/2270-number-of-ways-to-split-array.java
@@ -1,16 +1,15 @@
 class Solution {
     public int waysToSplitArray(int[] nums) {
+        long left = 0, right = 0;
         int res = 0;
-        long sum = 0;
-        for (int n : nums) {
-            sum += n;
+        long total = 0;
+        for(int n : nums){
+            total += n;
         }
-        long leftSum = 0, rightSum = 0;
-        for (int i = 0; i < nums.length - 1; i++) {
-            leftSum += nums[i];
-            rightSum = sum - leftSum;
-            if (leftSum >= rightSum)
-                res++;
+        for(int i = 0; i < nums.length - 1; i++){
+            left += nums[i];
+            right = total - left;
+            if(left >= right) res++;
         }
         return res;
     }
