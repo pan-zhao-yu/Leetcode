5a5e76831cd8935788c43a950fb67b7a7e6263c8
Joey <59043772+pan-zhao-yu@users.noreply.github.com>
2025-01-15 22:31:44 +0800

    Time: 0 ms (100%), Space: 44.4 MB (70.45%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
@@ -14,21 +14,18 @@
  * }
  */
 class Solution {
+    int res = 0;
     int count = 0;
-    int result = -1;
     public int kthSmallest(TreeNode root, int k) {
         dfs(root, k);
-        return result;
+        return res;
     }
-    
+
     private void dfs(TreeNode node, int k){
         if(node == null) return;
         dfs(node.left, k);
-                count++;
-        if(count == k){
-            result = node.val;
-            return;
-        }
+        count++;
+        if(count == k) res = node.val;
         dfs(node.right, k);
     }
 }
