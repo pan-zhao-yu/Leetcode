2861779150d9291d8b5790fa8b9fdda46b343f0f
Joey <59043772+pan-zhao-yu@users.noreply.github.com>
2025-01-18 20:21:14 +0800

    Time: 1 ms (100%), Space: 45.8 MB (89.61%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -6,22 +6,25 @@ class Solution {
         int left = 0;
         int right = nums1.length;
         double res = 0.0;
+
         while(left <= right){
-            int i = left + (right - left) / 2; //partition index for nums1
-            int j =  half - i; //index for nums2
-            int left1 = i > 0 ? nums1[i - 1] : Integer.MIN_VALUE;
-            int right1 = i < nums1.length ? nums1[i] : Integer.MAX_VALUE;
-            int left2 = j > 0 ? nums2[j - 1] : Integer.MIN_VALUE;
-            int right2 = j < nums2.length ? nums2[j] : Integer.MAX_VALUE;
-            if(left1 <= right2 && left2 <= right1){// partition is correct
+            int i = left + (right - left) / 2;
+            int j = half - i;
+
+            int left1 = i <= 0 ? Integer.MIN_VALUE : nums1[i - 1];
+            int right1 = i >= nums1.length ? Integer.MAX_VALUE : nums1[i];
+            int left2 = j <= 0 ? Integer.MIN_VALUE :nums2[j - 1];
+            int right2 = j >= nums2.length ? Integer.MAX_VALUE : nums2[j];
+
+            if(left1 <= right2 && left2 <= right1){
                 if(total % 2 == 0){
                     int n1 = Math.max(left1, left2);
                     int n2 = Math.min(right1, right2);
                     res = (n1 + n2) / 2.0;
+                    return res;
                 }else{
-                    res = Math.max(left1, left2);
+                    return Math.max(left1, left2);
                 }
-                return res;
             }else{
                 if(left1 > right2){
                     right = i - 1;
