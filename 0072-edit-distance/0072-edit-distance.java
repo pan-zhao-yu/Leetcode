324110369201143ca137b52464d77b7368810cc8
Joey <59043772+pan-zhao-yu@users.noreply.github.com>
2025-01-25 19:21:04 +0800

    Time: 5 ms (63.47%), Space: 45 MB (76.82%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -1,23 +1,16 @@
 class Solution {
     public int minDistance(String word1, String word2) {
-        if(word1.length() == 0 && word2.length() == 0) return 0;
-        if(word1.length() == 0) return word2.length();
-        if(word2.length() == 0) return word1.length();
         int[][] dp = new int[word1.length() + 1][word2.length() + 1];
         
-        for (int i = 0; i <= word1.length(); i++) {
-            dp[i][0] = i;  // Deleting all characters from word1 to match an empty word2
-        }
-        for (int j = 0; j <= word2.length(); j++) {
-            dp[0][j] = j;  // Inserting all characters from word2 to match an empty word1
-        }
-        
+        for(int i = 0; i <= word1.length(); i++) dp[i][0] = i;
+        for(int j = 0; j <= word2.length(); j++) dp[0][j] = j;
+
         for(int i = 1; i <= word1.length(); i++){
             for(int j = 1; j <= word2.length(); j++){
                 if(word1.charAt(i - 1) == word2.charAt(j - 1)){
                     dp[i][j] = dp[i - 1][j - 1];
                 }else{
-                    dp[i][j] = Math.min(dp[i - 1][j - 1] + 1, Math.min(dp[i - 1][j] + 1, dp[i][j - 1] + 1));
+                    dp[i][j] = Math.min(dp[i - 1][j - 1], Math.min(dp[i - 1][j], dp[i][j - 1])) + 1;
                 }
             }
         }
