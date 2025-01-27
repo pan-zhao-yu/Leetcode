b7f306793b0d80281dcc22313774e81d2e11f37c
Joey <59043772+pan-zhao-yu@users.noreply.github.com>
2025-01-27 15:42:27 +0800

    Time: 6 ms (28.71%), Space: 42.4 MB (30.27%) - LeetHub

--- 1049-last-stone-weight-ii/1049-last-stone-weight-ii.java
+++ 1049-last-stone-weight-ii/1049-last-stone-weight-ii.java
@@ -1,25 +1,21 @@
 class Solution {
     public int lastStoneWeightII(int[] stones) {
-        int sum = 0;
-        for(int stone : stones){
-            sum += stone;
+        int total = 0;
+        for (int n : stones) {
+            total += n;
         }
-        int target = sum / 2;
+        int target = total / 2;
         int[][] dp = new int[stones.length + 1][target + 1];
-        for(int i = 0; i <= stones.length; i++){
-            dp[i][0] = 0;
-        }
-        for(int i = 1; i <= stones.length; i++){
-            for(int j = 0; j <= target; j++){
-                if(j >= stones[i - 1]){
+        
+        for (int i = 1; i < stones.length + 1; i++) {
+            for (int j = 0; j < target + 1; j++) {
+                if (stones[i - 1] <= j) {
                     dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - stones[i - 1]] + stones[i - 1]);
-                }else{
+                } else {
                     dp[i][j] = dp[i - 1][j];
                 }
             }
         }
-        return sum - 2 * dp[stones.length][target];
+        return total - dp[stones.length][target] * 2;
     }
 }
-//dp[i][j] means the max sum we can achive by using the first i th element, in range j
-//dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - stones[i - 1]] + stones[i - 1]);
