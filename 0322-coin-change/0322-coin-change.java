50ffc34399336fb81137738f12be296d010a3ec6
Joey <59043772+pan-zhao-yu@users.noreply.github.com>
2025-02-05 18:02:40 +0800

    Time: 12 ms (92.69%), Space: 43.8 MB (98.74%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -16,31 +16,3 @@ class Solution {
         return dp[amount] == Integer.MAX_VALUE - 1 ? -1 : dp[amount];
     }
 }
-
-
-// class Solution {
-//     public int coinChange(int[] coins, int amount) {
-//         int[][] dp = new int[coins.length + 1][amount + 1];
-//         for(int i = 0; i <= coins.length; i++){
-//             dp[i][0] = 0;
-//         }
-//         for(int i = 0; i <= coins.length; i++){
-//             for(int j = 0; j <= amount; j++){
-//                 if(j > 0){
-//                     dp[i][j] = Integer.MAX_VALUE - 1;
-//                 }
-//             }
-//         }
-//         for(int i = 1; i <= coins.length; i++){
-//             for(int j = 0; j <= amount; j++){
-//                 if(j >= coins[i - 1]){
-//                     dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - coins[i - 1]] + 1);
-//                 }else{
-//                     dp[i][j] = dp[i - 1][j];
-//                 }
-//             }
-//         }
-//         return dp[coins.length][amount] == (Integer.MAX_VALUE - 1) ? -1 : dp[coins.length][amount];
-//     }
-// }
-//dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - coins[i]] + 1)
