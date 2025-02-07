9a63bcad148904b40b0175c3454db38025d1dea3
Joey <59043772+pan-zhao-yu@users.noreply.github.com>
2025-02-07 21:40:30 +0800

    Time: 10 ms (15.1%), Space: 45 MB (12.3%) - LeetHub

--- 0139-word-break/0139-word-break.java
+++ 0139-word-break/0139-word-break.java
@@ -1,18 +1,15 @@
 class Solution {
     public boolean wordBreak(String s, List<String> wordDict) {
-        HashSet<String> set = new HashSet<>(wordDict);
+        Set<String> set = new HashSet<>(wordDict);
         boolean[] valid = new boolean[s.length() + 1];
         valid[0] = true;
-
-        for (int i = 1; i <= s.length(); i++) {
-            for (int j = 0; j < i && !valid[i]; j++) {
-                if (set.contains(s.substring(j, i)) && valid[j]) {
+        for(int i = 1; i <= s.length(); i++){
+            for(int j = 0; j < i; j++){
+                if(set.contains(s.substring(j, i)) && valid[j]){
                     valid[i] = true;
                 }
             }
         }
         return valid[s.length()];
     }
-    
 }
-//dp[i] means the sub string from j to i can form wordDict
