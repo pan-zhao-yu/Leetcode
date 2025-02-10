ebb5b2d49286fa7a4121ed6e97a4328b5a56807b
Joey <59043772+pan-zhao-yu@users.noreply.github.com>
2025-02-10 22:54:07 +0800

    Time: 4 ms (90.42%), Space: 44.9 MB (93.45%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,29 +1,27 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        List<List<Integer>> reqMap = new ArrayList<>();
+        List<List<Integer>> prereqMap = new ArrayList<>();
         int[] visited = new int[numCourses];
         for(int i = 0; i < numCourses; i++){
-            reqMap.add(new ArrayList<>());
+            prereqMap.add(new ArrayList<>());
         }
         for(int i = 0; i < prerequisites.length; i++){
-            reqMap.get(prerequisites[i][0]).add(prerequisites[i][1]);
+            prereqMap.get(prerequisites[i][0]).add(prerequisites[i][1]);
         }
         for(int i = 0; i < numCourses; i++){
             if(visited[i] == 0){
-                if(isCircle(reqMap, visited, i)){
-                    return false;
-                }
+                if(isCircle(prereqMap, visited, i)) return false;
             }
-        }
-        return true;
+        }  
+        return true;      
     }
 
-    private boolean isCircle(List<List<Integer>> reqMap, int[] visited, int curr){
+    private boolean isCircle(List<List<Integer>> prereqMap, int[] visited, int curr){
         if(visited[curr] == 2) return true;
         visited[curr] = 2;
-        for(int i = 0; i < reqMap.get(curr).size(); i++){
-            if(visited[reqMap.get(curr).get(i)] != 1){
-                if(isCircle(reqMap, visited, reqMap.get(curr).get(i))) return true;
+        for(int i = 0; i < prereqMap.get(curr).size(); i++){
+            if(visited[prereqMap.get(curr).get(i)] != 1){
+                if(isCircle(prereqMap, visited, prereqMap.get(curr).get(i))) return true;
             }
         }
         visited[curr] = 1;
