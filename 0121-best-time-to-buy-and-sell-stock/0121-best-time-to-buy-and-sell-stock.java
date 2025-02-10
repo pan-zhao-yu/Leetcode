44d7dad9e7637b74840d9c117185cf644905649a
Joey <59043772+pan-zhao-yu@users.noreply.github.com>
2025-02-10 22:29:04 +0800

    Time: 2 ms (75.39%), Space: 61.8 MB (50.72%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,14 +1,26 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int[][] dp = new int[prices.length][2];
-        dp[0][0] = 0;
-        dp[0][1] = -prices[0];
-        for(int i = 1; i < prices.length; i++){
-            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
-            dp[i][1] = Math.max(dp[i - 1][1], -prices[i]);
+        int left = Integer.MAX_VALUE;
+        int profit = 0;
+        for(int i = 0; i < prices.length; i++){
+            left = Math.min(left, prices[i]);
+            profit = Math.max(profit, prices[i] - left);
         }
-        return dp[prices.length - 1][0];
+        return profit;
     }
 }
-//dp[i][0] means the max cash we can have if sell or not holding any stock on ith day
-//dp[i][1] means the max cash we can have if buy or keep holding stock on ith day;
+
+// class Solution {
+//     public int maxProfit(int[] prices) {
+//         int[][] dp = new int[prices.length][2];
+//         dp[0][0] = 0;
+//         dp[0][1] = -prices[0];
+//         for(int i = 1; i < prices.length; i++){
+//             dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
+//             dp[i][1] = Math.max(dp[i - 1][1], -prices[i]);
+//         }
+//         return dp[prices.length - 1][0];
+//     }
+// }
+// //dp[i][0] means the max cash we can have if sell or not holding any stock on ith day
+// //dp[i][1] means the max cash we can have if buy or keep holding stock on ith day;
