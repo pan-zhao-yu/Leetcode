284cab714a18f653d3ef40345dd225670958acf8
Joey <59043772+pan-zhao-yu@users.noreply.github.com>
2025-02-10 22:05:05 +0800

    Time: 3 ms (99.83%), Space: 45.1 MB (83.04%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,17 +1,16 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        List<List<Integer>> adjacent = new ArrayList<>();
-        for(int i = 0; i < numCourses; i++){
-            adjacent.add(new ArrayList<>());
-        }
-        for(int i = 0; i< prerequisites.length; i++){
-            adjacent.get(prerequisites[i][0]).add(prerequisites[i][1]);
-        }
-        
+        List<List<Integer>> reqMap = new ArrayList<>();
         int[] visited = new int[numCourses];
         for(int i = 0; i < numCourses; i++){
+            reqMap.add(new ArrayList<>());
+        }
+        for(int i = 0; i < prerequisites.length; i++){
+            reqMap.get(prerequisites[i][0]).add(prerequisites[i][1]);
+        }
+        for(int i = 0; i < numCourses; i++){
             if(visited[i] == 0){
-                if(isCyclic(adjacent, visited, i)){
+                if(isCircle(reqMap, visited, i)){
                     return false;
                 }
             }
@@ -19,61 +18,15 @@ class Solution {
         return true;
     }
 
-    private boolean isCyclic(List<List<Integer>> adjacent, int[] visited, int curr){
-        if(visited[curr] == 2){
-            return true;
-        }
+    private boolean isCircle(List<List<Integer>> reqMap, int[] visited, int curr){
+        if(visited[curr] == 2) return true;
         visited[curr] = 2;
-        for(int i = 0; i < adjacent.get(curr).size(); i++){
-            if(visited[adjacent.get(curr).get(i)] != 1){
-                if(isCyclic(adjacent, visited, adjacent.get(curr).get(i))){
-                    return true;
-                }
+        for(int i = 0; i < reqMap.get(curr).size(); i++){
+            if(visited[reqMap.get(curr).get(i)] != 1){
+                if(isCircle(reqMap, visited, reqMap.get(curr).get(i))) return true;
             }
         }
         visited[curr] = 1;
         return false;
     }
 }
-
-
-// class Solution {
-
-//     public boolean canFinish(int numCourses, int[][] prerequisites) {
-//         List<List<Integer>> adj = new ArrayList<>();
-//         for (int i = 0; i < numCourses; i++) {
-//             adj.add(new ArrayList<>());
-//         }
-
-//         for (int i = 0; i < prerequisites.length; i++) {
-//             adj.get(prerequisites[i][0]).add(prerequisites[i][1]);
-//         }
-
-//         int[] visited = new int[numCourses];
-//         for (int i = 0; i < numCourses; i++) {
-//             if (visited[i] == 0) {
-//                 if (isCyclic(adj, visited, i)) {
-//                     return false;
-//                 }
-//             }
-//         }
-//         return true;
-//     }
-
-//     private boolean isCyclic(List<List<Integer>> adj, int[] visited, int curr) {
-//         if (visited[curr] == 2) {
-//             return true;
-//         }
-
-//         visited[curr] = 2;
-//         for (int i = 0; i < adj.get(curr).size(); i++) {
-//             if (visited[adj.get(curr).get(i)] != 1) {
-//                 if (isCyclic(adj, visited, adj.get(curr).get(i))) {
-//                     return true;
-//                 }
-//             }
-//         }
-//         visited[curr] = 1;
-//         return false;
-//     }
-// }
