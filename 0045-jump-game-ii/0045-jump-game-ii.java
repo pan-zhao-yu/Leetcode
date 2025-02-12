171eeb008ddfdbf5167ece6ce2eccd26bc1984be
Joey <59043772+pan-zhao-yu@users.noreply.github.com>
2025-02-12 21:54:30 +0800

    Time: 12 ms (22.41%), Space: 45.8 MB (13.27%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -1,27 +1,43 @@
-public class Solution {
+class Solution {
     public int jump(int[] nums) {
-        if (nums.length <= 1) {
-            return 0;
-        }
-
-        int jumps = 1, maxReach = nums[0], steps = nums[0];
-        for (int i = 1; i < nums.length; i++) {
-            if (i == nums.length - 1) {
-                return jumps;
+        int res = 0;
+        int left = 0, right = 0;
+        while(right < nums.length - 1){
+            int farest = 0;
+            for(int i = left; i < right + 1; i++){
+                farest = Math.max(nums[i] + i, farest);
             }
-
-            maxReach = Math.max(maxReach, i + nums[i]);
-            steps--;
-
-            if (steps == 0) {
-                jumps++;
-                if (i >= maxReach) {
-                    return -1; // Cannot reach the end
-                }
-                steps = maxReach - i;
-            }
+            right = farest;
+            res++;
         }
-
-        return jumps;
+        return res;
     }
 }
+
+// public class Solution {
+//     public int jump(int[] nums) {
+//         if (nums.length <= 1) {
+//             return 0;
+//         }
+
+//         int jumps = 1, maxReach = nums[0], steps = nums[0];
+//         for (int i = 1; i < nums.length; i++) {
+//             if (i == nums.length - 1) {
+//                 return jumps;
+//             }
+
+//             maxReach = Math.max(maxReach, i + nums[i]);
+//             steps--;
+
+//             if (steps == 0) {
+//                 jumps++;
+//                 if (i >= maxReach) {
+//                     return -1; // Cannot reach the end
+//                 }
+//                 steps = maxReach - i;
+//             }
+//         }
+
+//         return jumps;
+//     }
+// }
