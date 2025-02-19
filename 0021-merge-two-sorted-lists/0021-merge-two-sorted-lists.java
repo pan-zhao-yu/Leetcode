a3a96bef6b2349ed2a97eb0e36d9b82bf5d9e3e8
Joey <59043772+pan-zhao-yu@users.noreply.github.com>
2025-02-19 19:29:15 +0800

    Time: 1 ms (4.2%), Space: 42.9 MB (14.95%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -1,36 +1,36 @@
 /**
  * Definition for singly-linked list.
  * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        
-        ListNode result = new ListNode();
-        ListNode dummy = result;
-        dummy = result;
-        while(list1 != null && list2 != null){
-            if(list1.val < list2.val){
-                result.next = list1;
+        ListNode res = new ListNode(0);
+        ListNode tail = res;
+        while (list1 != null && list2 != null) {
+            int n1 = list1.val;
+            int n2 = list2.val;
+            if (n1 < n2) {
+                ListNode curr = new ListNode(n1);
+                tail.next = curr;
                 list1 = list1.next;
-                result = result.next;
-            }else{
-                result.next = list2;
+            } else {
+                ListNode curr = new ListNode(n2);
+                tail.next = curr;
                 list2 = list2.next;
-                result = result.next;
             }
+            tail = tail.next;
         }
-        if(list1 != null){
-            result.next = list1;
+        if (list1 != null) {
+            tail.next = list1;
+        } else if (list2 != null) {
+            tail.next = list2;
         }
-        if(list2 != null){
-            result.next = list2;
-        }
-        return dummy.next;
+        return res.next;
     }
 }
