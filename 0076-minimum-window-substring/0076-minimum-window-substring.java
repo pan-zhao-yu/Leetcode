381a0479b2e728873282d880478221dff0fbc1b0
Joey <59043772+pan-zhao-yu@users.noreply.github.com>
2025-05-23 03:04:26 +1000

    Time: 18 ms (57.75%), Space: 45.1 MB (59.86%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,33 +1,31 @@
 class Solution {
     public String minWindow(String s, String t) {
-        int left = 0, right = 0, miniLen = Integer.MAX_VALUE, miniLeft = s.length(), miniRight = s.length();
+        int l = 0, r = 0;
         Map<Character, Integer> tMap = new HashMap<>();
         for(char c : t.toCharArray()){
             tMap.put(c, tMap.getOrDefault(c, 0) + 1);
         }
         int need = tMap.size();
-        int found = 0;
-        Map<Character, Integer> window = new HashMap<>();
-        while(right < s.length()){
-            window.put(s.charAt(right), window.getOrDefault(s.charAt(right), 0) + 1);
-            if(tMap.containsKey(s.charAt(right)) && window.get(s.charAt(right)).intValue() == tMap.get(s.charAt(right)).intValue()){
-                found++;
+        int miniLen = Integer.MAX_VALUE;
+        int left = 0;
+        while(r < s.length()){
+            if(tMap.containsKey(s.charAt(r))){
+                tMap.put(s.charAt(r), tMap.get(s.charAt(r)) - 1);
+                if(tMap.get(s.charAt(r)) == 0) need--;
             }
-            right++;
-            while(found == need){
-                if(right - left < miniLen){
-                    miniLeft = left;
-                    miniRight = right;
-                    miniLen = right - left;
+            r++;
+            while(need == 0){
+                if(r - l < miniLen){
+                    miniLen = r - l;
+                    left = l;
                 }
-                window.put(s.charAt(left), window.get(s.charAt(left)) - 1);
-                if(tMap.containsKey(s.charAt(left)) && window.get(s.charAt(left)) < tMap.get(s.charAt(left))){
-                    found--;
+                if(tMap.containsKey(s.charAt(l))){
+                    tMap.put(s.charAt(l), tMap.get(s.charAt(l)) + 1);
+                    if(tMap.get(s.charAt(l)) > 0) need++;
                 }
-                left++;
+                l++;
             }
-            
         }
-        return miniLen > s.length() ? "" : s.substring(miniLeft, miniRight);
+        return miniLen == Integer.MAX_VALUE ? "" : s.substring(left, left + miniLen);
     }
 }
