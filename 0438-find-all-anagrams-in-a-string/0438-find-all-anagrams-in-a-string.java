d75d9988b04b2eb6c6d4339f1ec58112f101423f
Joey <59043772+pan-zhao-yu@users.noreply.github.com>
2025-05-24 20:09:34 +1000

    Time: 13 ms (54.7%), Space: 44.5 MB (98.54%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
@@ -1,41 +1,29 @@
 class Solution {
-
-    static boolean isequal(int counta[],int countb[]){
-       
-        boolean ans=true;
-        for(int i=0;i<26;i++){
-            if(counta[i]!=countb[i]){
-                ans= false;
-                break;
-            }
-        }
-        return ans;
-
-    }
-
     public List<Integer> findAnagrams(String s, String p) {
-        int k=p.length();
-        List<Integer> l=new ArrayList<>();
-        if(s.length()<k){
-            return l;
+        List<Integer> res = new ArrayList<>();
+        int[] pMap = new int[26];
+        int[] window = new int[26];
+        for(char c : p.toCharArray()){
+            pMap[c - 'a']++;
         }
-        
-        int counts[]=new int[26];
-        int countp[]=new int[26];
-        for(int i=0;i<k;i++){
-            counts[s.charAt(i)-'a']++;
-            countp[p.charAt(i)-'a']++;
-        }
-        if(isequal(counts,countp)){
-            l.add(0);
-        }
-        for(int i=k;i<s.length();i++){
-            counts[s.charAt(i)-'a']++;
-            counts[s.charAt(i-k)-'a']--;
-            if(isequal(counts,countp)){
-                l.add(i-k+1);
+        int r = 0;
+        while(r < s.length()){
+            window[s.charAt(r) - 'a']++;
+            if(r + 1 - p.length() >= 0){
+                if(isAnagram(window, pMap)){
+                    res.add(r + 1 - p.length());
+                }
+                window[s.charAt(r + 1 - p.length()) - 'a']--;
             }
+
+            r++;
         }
-        return l;
+        return res;
+    }
+    private boolean isAnagram(int[] window, int[] pMap){
+        for(int i = 0; i < window.length; i++){
+            if(window[i] != pMap[i]) return false;
+        }
+        return true;
     }
 }
