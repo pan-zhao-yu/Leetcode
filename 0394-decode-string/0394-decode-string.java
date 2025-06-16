a9bc6d59007474b3048ede0a804f5d321e16b35b
Joey <59043772+pan-zhao-yu@users.noreply.github.com>
2025-06-17 00:10:53 +1000

    Time: 0 ms (100%), Space: 41.6 MB (67.95%) - LeetHub

--- 0394-decode-string/0394-decode-string.java
+++ 0394-decode-string/0394-decode-string.java
@@ -1,3 +1,4 @@
+// Recursive solution
 class Solution {
     int index = 0;
     public String decodeString(String s) {
@@ -22,3 +23,34 @@ class Solution {
         return result.toString();
     }
 }
+
+// Stack solution
+
+// class Solution {
+//     public String decodeString(String s) {
+//         Deque<Integer> countStack = new ArrayDeque<>();
+//         Deque<StringBuilder> stringStack = new ArrayDeque<>();
+//         StringBuilder sb = new StringBuilder();
+//         int k = 0;
+
+//         for(char c : s.toCharArray()){
+//             if(Character.isDigit(c)) k = k * 10 + (c - '0');
+//             else if(c == '['){
+//                 countStack.push(k);
+//                 k = 0;
+//                 stringStack.push(sb);
+//                 sb = new StringBuilder();
+//             }else if(c == ']'){
+//                 int repeat = countStack.pop();
+//                 StringBuilder curr = stringStack.pop();
+//                 for(int i = 0; i < repeat; i++){
+//                     curr.append(sb);
+//                 }
+//                 sb = curr;
+//             }else{
+//                 sb.append(c);
+//             }
+//         }
+//         return sb.toString();
+//     }
+// }
