8125aea9e2d22010a59c478006f4423569860a86
Joey <59043772+pan-zhao-yu@users.noreply.github.com>
2025-06-18 20:32:54 +1000

    Time: 1 ms (100%), Space: 57.5 MB (39.92%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.java
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -15,3 +15,18 @@ class Solution {
         return n +1;
     }
 }
+
+
+// class Solution {
+//     public int firstMissingPositive(int[] nums) {
+//         Set<Integer> set = new HashSet<>();
+//         for(int n : nums){
+//             set.add(n);
+//         }
+//         int i = 1;
+//         for(; i <= nums.length; i++){
+//             if(!set.contains(i)) return i;
+//         }
+//         return i;
+//     }
+// }
