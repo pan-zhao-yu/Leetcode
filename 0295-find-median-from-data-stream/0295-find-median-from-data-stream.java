d23be64b436bb9576ae5b1058bf2b9f831767d73
Joey <59043772+pan-zhao-yu@users.noreply.github.com>
2025-06-24 20:30:57 +1000

    Time: 126 ms (38.7%), Space: 63.9 MB (29.12%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.java
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.java
@@ -1,25 +1,19 @@
 class MedianFinder {
-    PriorityQueue<Integer> min;
-    PriorityQueue<Integer> max;
+    PriorityQueue<Integer> minHeap, maxHeap;
     public MedianFinder() {
-        min = new PriorityQueue<>();
-        max = new PriorityQueue<>((a,  b) -> b - a);
+        minHeap = new PriorityQueue<>();
+        maxHeap = new PriorityQueue<>((a, b) -> b - a); //(Collection.revereseOrder());
     }
     
     public void addNum(int num) {
-        max.offer(num);
-        min.offer(max.poll());
-        if(min.size() > max.size()){
-            max.offer(min.poll());
-        }
+        maxHeap.offer(num);
+        minHeap.offer(maxHeap.poll());
+        if(minHeap.size() > maxHeap.size()) maxHeap.offer(minHeap.poll());
     }
     
     public double findMedian() {
-        if(max.size() == min.size()){
-            return (max.peek() + min.peek()) / 2.0;
-        }else{
-            return max.peek();
-        }
+        if((minHeap.size() + maxHeap.size()) % 2 == 0) return (minHeap.peek() + maxHeap.peek()) / 2.0;
+        else return maxHeap.peek();
     }
 }
 
@@ -29,24 +23,3 @@ class MedianFinder {
  * obj.addNum(num);
  * double param_2 = obj.findMedian();
  */
-
-
-// class MedianFinder {
-//     List<Integer> nums;
-//     public MedianFinder() {
-//         nums = new ArrayList();
-        
-//     }
-    
-//     public void addNum(int num) {
-//         nums.add(num);
-//         Collections.sort(nums);
-//     }
-    
-//     public double findMedian() {
-//         if(nums.size() % 2 == 0){
-//             return (nums.get(nums.size() / 2) + nums.get(nums.size() / 2 - 1) ) / 2.0;
-//         }
-//         return nums.get(nums.size() /2);
-//     }
-// }
