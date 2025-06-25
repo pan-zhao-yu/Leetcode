6dec5918cb0ae0239f936660853f60e358fe2d3e
Joey <59043772+pan-zhao-yu@users.noreply.github.com>
2025-06-25 14:20:47 +1000

    Time: 0 ms (100%), Space: 45.6 MB (63.8%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,27 +1,96 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        Set<Integer> rowSet = new HashSet<>();
-        Set<Integer> colSet = new HashSet<>();
+        int rows = matrix.length;
+        int cols = matrix[0].length;
 
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[0].length; j++) {
-                if (matrix[i][j] == 0) {
-                    rowSet.add(i);
-                    colSet.add(j);
+        boolean firstRowHasZero = false;
+        boolean firstColHasZero = false;
+
+        // Check if the first row contains zero
+        for (int c = 0; c < cols; c++) {
+            if (matrix[0][c] == 0) {
+                firstRowHasZero = true;
+                break;
+            }
+        }
+
+        // Check if the first column contains zero
+        for (int r = 0; r < rows; r++) {
+            if (matrix[r][0] == 0) {
+                firstColHasZero = true;
+                break;
+            }
+        }
+
+        // Use the first row and column as markers
+        for (int r = 1; r < rows; r++) {
+            for (int c = 1; c < cols; c++) {
+                if (matrix[r][c] == 0) {
+                    matrix[r][0] = 0;
+                    matrix[0][c] = 0;
                 }
             }
         }
 
-        for (int i : rowSet) {
-            for (int j = 0; j < matrix[0].length; j++) {
-                matrix[i][j] = 0;
+        // Set the marked rows to zero
+        for (int r = 1; r < rows; r++) {
+            if (matrix[r][0] == 0) {
+                for (int c = 1; c < cols; c++) {
+                    matrix[r][c] = 0;
+                }
             }
         }
 
-        for (int j : colSet) {
-            for (int i = 0; i < matrix.length; i++) {
-                matrix[i][j] = 0;
+        // Set the marked columns to zero
+        for (int c = 1; c < cols; c++) {
+            if (matrix[0][c] == 0) {
+                for (int r = 1; r < rows; r++) {
+                    matrix[r][c] = 0;
+                }
             }
         }
+
+        // Set the first row to zero if needed
+        if (firstRowHasZero) {
+            for (int c = 0; c < cols; c++) {
+                matrix[0][c] = 0;
+            }
+        }
+
+        // Set the first column to zero if needed
+        if (firstColHasZero) {
+            for (int r = 0; r < rows; r++) {
+                matrix[r][0] = 0;
+            }
+        }        
     }
 }
+
+
+// class Solution {
+//     public void setZeroes(int[][] matrix) {
+//         Set<Integer> rowSet = new HashSet<>();
+//         Set<Integer> colSet = new HashSet<>();
+
+//         for (int i = 0; i < matrix.length; i++) {
+//             for (int j = 0; j < matrix[0].length; j++) {
+//                 if (matrix[i][j] == 0) {
+//                     rowSet.add(i);
+//                     colSet.add(j);
+//                 }
+//             }
+//         }
+
+//         for (int i : rowSet) {
+//             for (int j = 0; j < matrix[0].length; j++) {
+//                 matrix[i][j] = 0;
+//             }
+//         }
+
+//         for (int j : colSet) {
+//             for (int i = 0; i < matrix.length; i++) {
+//                 matrix[i][j] = 0;
+//             }
+//         }
+//     }
+// }
