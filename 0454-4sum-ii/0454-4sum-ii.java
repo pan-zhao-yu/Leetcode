ebdd88a4ab239f0271aa376db05aaaad016f5054
Joey <59043772+pan-zhao-yu@users.noreply.github.com>
2025-07-01 00:11:57 +1000

    Time: 106 ms (95.33%), Space: 44.8 MB (90.4%) - LeetHub

--- 0454-4sum-ii/0454-4sum-ii.java
+++ 0454-4sum-ii/0454-4sum-ii.java
@@ -1,19 +1,17 @@
 class Solution {
     public int fourSumCount(int[] nums1, int[] nums2, int[] nums3, int[] nums4) {
         Map<Integer, Integer> map = new HashMap<>();
-        int result = 0;
-        for(int a : nums1){
-            for(int b : nums2){
-                map.put(a + b, map.getOrDefault(a + b, 0) + 1);
+        int res = 0;
+        for(int n : nums1){
+            for(int x : nums2){
+                map.put(n + x, map.getOrDefault(n + x, 0) + 1);
             }
         }
-        for(int c : nums3){
-            for(int d : nums4){
-                if(map.containsKey(- c - d)){
-                    result += map.getOrDefault(- c - d, 0);
-                }
+        for(int n : nums3){
+            for(int x : nums4){
+                res += map.getOrDefault(0 - n - x, 0);
             }
         }
-        return result;
+        return res;
     }
 }
