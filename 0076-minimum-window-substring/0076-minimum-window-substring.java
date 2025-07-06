7633f77d0d9433a319781836810ef316184fd221
Joey <59043772+pan-zhao-yu@users.noreply.github.com>
2025-07-07 04:58:29 +1000

    Time: 18 ms (57.42%), Space: 45 MB (60.29%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,13 +1,13 @@
 class Solution {
     public String minWindow(String s, String t) {
-        int l = 0, r = 0;
         Map<Character, Integer> tMap = new HashMap<>();
         for(char c : t.toCharArray()){
             tMap.put(c, tMap.getOrDefault(c, 0) + 1);
         }
+        int l = 0, r = 0;
         int need = tMap.size();
-        int miniLen = Integer.MAX_VALUE;
         int left = 0;
+        int min = Integer.MAX_VALUE;
         while(r < s.length()){
             if(tMap.containsKey(s.charAt(r))){
                 tMap.put(s.charAt(r), tMap.get(s.charAt(r)) - 1);
@@ -15,9 +15,9 @@ class Solution {
             }
             r++;
             while(need == 0){
-                if(r - l < miniLen){
-                    miniLen = r - l;
+                if(r - l < min){
                     left = l;
+                    min = r - l;
                 }
                 if(tMap.containsKey(s.charAt(l))){
                     tMap.put(s.charAt(l), tMap.get(s.charAt(l)) + 1);
@@ -26,6 +26,6 @@ class Solution {
                 l++;
             }
         }
-        return miniLen == Integer.MAX_VALUE ? "" : s.substring(left, left + miniLen);
+        return min == Integer.MAX_VALUE ? "" : s.substring(left, left + min);
     }
 }
