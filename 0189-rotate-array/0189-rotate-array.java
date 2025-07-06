6e947a9755029d4cd2eb8835e1bd359bc2708334
Joey <59043772+pan-zhao-yu@users.noreply.github.com>
2025-07-07 05:25:15 +1000

    Time: 2 ms (26.65%), Space: 57.1 MB (85.87%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,3 +1,23 @@
+class Solution {
+    public void rotate(int[] nums, int k) {
+        int[] res = new int[nums.length];
+        k = k % nums.length;
+        for(int i = k; i < nums.length; i++){
+            res[i] = nums[i - k];
+        }
+        for(int i = 0; i < k; i++){
+            res[i] = nums[nums.length - k + i];
+        }
+        for(int i = 0; i < nums.length; i++){
+            nums[i] = res[i];
+        }
+    }
+}
+
+
+
+
+
 // class Solution {
 //     public void rotate(int[] nums, int k) {
 //         k %= nums.length;
@@ -17,14 +37,14 @@
 //     }
 // }
 
-class Solution {
-    public void rotate(int[] nums, int k) {
-        int[] res = new int[nums.length];
-        for(int i = 0; i < nums.length; i++){
-            res[(i + k) % nums.length] = nums[i];
-        }
-        for(int j = 0; j < nums.length; j++){
-            nums[j] = res[j];
-        }
-    }
-}
+// class Solution {
+//     public void rotate(int[] nums, int k) {
+//         int[] res = new int[nums.length];
+//         for(int i = 0; i < nums.length; i++){
+//             res[(i + k) % nums.length] = nums[i];
+//         }
+//         for(int j = 0; j < nums.length; j++){
+//             nums[j] = res[j];
+//         }
+//     }
+// }
