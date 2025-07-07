3b54743f0f5eec089c74f13b8b9af028836cdcf1
Joey <59043772+pan-zhao-yu@users.noreply.github.com>
2025-07-08 05:11:27 +1000

    Time: 1 ms (72.39%), Space: 45.5 MB (91.95%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,96 +1,43 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        int rows = matrix.length;
-        int cols = matrix[0].length;
-
-        boolean firstRowHasZero = false;
-        boolean firstColHasZero = false;
-
-        // Check if the first row contains zero
-        for (int c = 0; c < cols; c++) {
-            if (matrix[0][c] == 0) {
-                firstRowHasZero = true;
-                break;
-            }
+        boolean rowHasZero = false, colHasZero = false;
+        for(int i = 0; i < matrix.length; i++){
+            if(matrix[i][0] == 0) colHasZero = true;
         }
-
-        // Check if the first column contains zero
-        for (int r = 0; r < rows; r++) {
-            if (matrix[r][0] == 0) {
-                firstColHasZero = true;
-                break;
-            }
+        for(int i = 0; i < matrix[0].length; i++){
+            if(matrix[0][i] == 0) rowHasZero = true;
         }
-
-        // Use the first row and column as markers
-        for (int r = 1; r < rows; r++) {
-            for (int c = 1; c < cols; c++) {
-                if (matrix[r][c] == 0) {
-                    matrix[r][0] = 0;
-                    matrix[0][c] = 0;
+        for(int i = 1; i < matrix.length; i++){
+            for(int j = 1; j < matrix[0].length; j++){
+                if(matrix[i][j] == 0){
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
                 }
             }
         }
-
-        // Set the marked rows to zero
-        for (int r = 1; r < rows; r++) {
-            if (matrix[r][0] == 0) {
-                for (int c = 1; c < cols; c++) {
-                    matrix[r][c] = 0;
+        for(int i = 1; i < matrix.length; i++){
+            if(matrix[i][0] == 0){
+                for(int j = 1; j < matrix[0].length; j++){
+                    matrix[i][j] = 0;
                 }
             }
         }
-
-        // Set the marked columns to zero
-        for (int c = 1; c < cols; c++) {
-            if (matrix[0][c] == 0) {
-                for (int r = 1; r < rows; r++) {
-                    matrix[r][c] = 0;
+        for(int i = 1; i < matrix[0].length; i++){
+            if(matrix[0][i] == 0){
+                for(int j = 1; j < matrix.length; j++){
+                    matrix[j][i] = 0;
                 }
             }
         }
-
-        // Set the first row to zero if needed
-        if (firstRowHasZero) {
-            for (int c = 0; c < cols; c++) {
-                matrix[0][c] = 0;
+        if(rowHasZero){
+            for(int i = 0; i < matrix[0].length; i++){
+                matrix[0][i] = 0;
             }
         }
-
-        // Set the first column to zero if needed
-        if (firstColHasZero) {
-            for (int r = 0; r < rows; r++) {
-                matrix[r][0] = 0;
+        if(colHasZero){
+            for(int i = 0; i < matrix.length; i++){
+                matrix[i][0] = 0;
             }
-        }        
+        }
     }
 }
-
-
-// class Solution {
-//     public void setZeroes(int[][] matrix) {
-//         Set<Integer> rowSet = new HashSet<>();
-//         Set<Integer> colSet = new HashSet<>();
-
-//         for (int i = 0; i < matrix.length; i++) {
-//             for (int j = 0; j < matrix[0].length; j++) {
-//                 if (matrix[i][j] == 0) {
-//                     rowSet.add(i);
-//                     colSet.add(j);
-//                 }
-//             }
-//         }
-
-//         for (int i : rowSet) {
-//             for (int j = 0; j < matrix[0].length; j++) {
-//                 matrix[i][j] = 0;
-//             }
-//         }
-
-//         for (int j : colSet) {
-//             for (int i = 0; i < matrix.length; i++) {
-//                 matrix[i][j] = 0;
-//             }
-//         }
-//     }
-// }
