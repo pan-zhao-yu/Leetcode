9887d89a5b493ac3d3801806534e5f1d2f67b705
Joey <59043772+pan-zhao-yu@users.noreply.github.com>
2025-07-12 18:19:10 +1000

    Time: 0 ms (100%), Space: 41.7 MB (18.91%) - LeetHub

--- 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.java
+++ 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.java
@@ -12,18 +12,36 @@ class Solution {
     public ListNode swapPairs(ListNode head) {
         ListNode dummy = new ListNode(0, head);
         ListNode curr = dummy;
-        ListNode prev = dummy;
         while(curr.next != null && curr.next.next != null){
-            ListNode next = curr.next.next.next;
             ListNode n1 = curr.next;
             ListNode n2 = curr.next.next;
-            prev.next = n2;
-            curr = curr.next;
-            curr.next = n1;
-            curr = curr.next;
-            curr.next = next;
-            prev = curr;
+            //swap
+            n1.next = n2.next;
+            n2.next = n1;
+            curr.next = n2;
+            //move curr
+            curr = n1;
         }
         return dummy.next;
     }
 }
+
+// class Solution {
+//     public ListNode swapPairs(ListNode head) {
+//         ListNode dummy = new ListNode(0, head);
+//         ListNode curr = dummy;
+//         ListNode prev = dummy;
+//         while(curr.next != null && curr.next.next != null){
+//             ListNode next = curr.next.next.next;
+//             ListNode n1 = curr.next;
+//             ListNode n2 = curr.next.next;
+//             prev.next = n2;
+//             curr = curr.next;
+//             curr.next = n1;
+//             curr = curr.next;
+//             curr.next = next;
+//             prev = curr;
+//         }
+//         return dummy.next;
+//     }
+// }
