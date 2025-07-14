59b956fe8a5bda8c167cdd03834e159e562d266c
Joey <59043772+pan-zhao-yu@users.noreply.github.com>
2025-07-15 01:46:47 +1000

    Time: 0 ms (100%), Space: 44.1 MB (98.84%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
@@ -13,38 +13,22 @@ class Node {
 }
 */
 
-public class Solution {
+class Solution {
     public Node copyRandomList(Node head) {
-        if (head == null) return null;
-
-        // Step 1: Creating a copy of each node and interweaving
-        Node current = head;
-        while (current != null) {
-            Node copy = new Node(current.val);
-            copy.next = current.next;
-            current.next = copy;
-            current = copy.next;
+        Map<Node, Node> map = new HashMap<>();
+        Node curr = head;
+        while(curr != null){
+            map.put(curr, new Node(curr.val));
+            curr = curr.next;
         }
-
-        // Step 2: Updating random pointers of the copied nodes
-        current = head;
-        while (current != null) {
-            if (current.random != null) {
-                current.next.random = current.random.next;
-            }
-            current = current.next.next;
+        curr = head;
+        Node res = map.get(head);
+        while(curr != null){
+            res.next = map.get(curr.next);
+            res.random = map.get(curr.random);
+            res = res.next;
+            curr = curr.next;
         }
-
-        // Step 3: Separating the original and the copied lists
-        Node dummy = new Node(0);
-        Node copyCurrent = dummy, originalCurrent = head;
-        while (originalCurrent != null) {
-            copyCurrent.next = originalCurrent.next;
-            originalCurrent.next = originalCurrent.next.next;
-            copyCurrent = copyCurrent.next;
-            originalCurrent = originalCurrent.next;
-        }
-
-        return dummy.next;
+        return map.get(head);
     }
 }
