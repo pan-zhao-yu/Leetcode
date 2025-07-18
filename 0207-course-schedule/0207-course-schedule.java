5a5d0b9f9440b67ba8f036c9c6d3cf25572dec68
Joey <59043772+pan-zhao-yu@users.noreply.github.com>
2025-07-19 01:19:03 +1000

    Time: 6 ms (73.45%), Space: 45.5 MB (47.64%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,29 +1,27 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        List<List<Integer>> adjacent = new ArrayList<>();
-        int[] visited = new int[numCourses];
+        List<List<Integer>> graph = new ArrayList<>();
         for(int i = 0; i < numCourses; i++){
-            adjacent.add(new ArrayList<>());
+            graph.add(new ArrayList<>());
         }
-        for(int[] prerequisite : prerequisites){
-            adjacent.get(prerequisite[0]).add(prerequisite[1]);
+        int[] indegree = new int[numCourses];
+        for(int[] pre : prerequisites){
+            graph.get(pre[1]).add(pre[0]);
+            indegree[pre[0]]++;
         }
+        Queue<Integer> q = new ArrayDeque<>();
         for(int i = 0; i < numCourses; i++){
-            if(visited[i] == 0){
-                if(isCircle(adjacent, visited, i)) return false;
+            if(indegree[i] == 0) q.add(i);
+        }
+        int count = 0;
+        while(!q.isEmpty()){
+            int c = q.poll();
+            count++;
+            for(int pre : graph.get(c)){
+                indegree[pre]--;
+                if(indegree[pre] == 0) q.add(pre);
             }
         }
-        return true;
-    }
-
-    private boolean isCircle(List<List<Integer>> adjacent, int[] visited, int curr){
-        if(visited[curr] == 2) return true;
-        if(visited[curr] == 1) return false;
-        visited[curr] = 2;
-        for(int pre : adjacent.get(curr)){
-            if(isCircle(adjacent, visited, pre)) return true;
-        }
-        visited[curr] = 1;
-        return false;
+        return count == numCourses;
     }
 }
