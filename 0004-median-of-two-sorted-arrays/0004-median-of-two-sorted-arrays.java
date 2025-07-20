b7b6cfe5997df2188846b9eef1940788a002cdf6
Joey <59043772+pan-zhao-yu@users.noreply.github.com>
2025-07-21 03:39:30 +1000

    Time: 1 ms (100%), Space: 46.5 MB (5.43%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,35 +1,31 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
         if(nums1.length > nums2.length) return findMedianSortedArrays(nums2, nums1);
-        int total = nums1.length + nums2.length;
-        int half = (total + 1) / 2;
-        int left = 0;
-        int right = nums1.length;
+        int totalLen = nums1.length + nums2.length;
+        int half = (totalLen + 1) / 2;
+        int left = 0, right = nums1.length;
         double res = 0.0;
-
         while(left <= right){
-            int i = left + (right - left) / 2;
-            int j = half - i;
-
-            int left1 = i <= 0 ? Integer.MIN_VALUE : nums1[i - 1];
-            int right1 = i >= nums1.length ? Integer.MAX_VALUE : nums1[i];
-            int left2 = j <= 0 ? Integer.MIN_VALUE :nums2[j - 1];
-            int right2 = j >= nums2.length ? Integer.MAX_VALUE : nums2[j];
-
+            int mid1 = left + (right - left) / 2;
+            int mid2 = half - mid1;
+            int left1 = mid1 <= 0 ? Integer.MIN_VALUE : nums1[mid1 - 1];
+            int right1 = mid1 >= nums1.length ? Integer.MAX_VALUE : nums1[mid1];
+            int left2 = mid2 <= 0 ? Integer.MIN_VALUE : nums2[mid2 - 1];
+            int right2 = mid2 >= nums2.length ? Integer.MAX_VALUE : nums2[mid2];
             if(left1 <= right2 && left2 <= right1){
-                if(total % 2 == 0){
-                    int n1 = Math.max(left1, left2);
-                    int n2 = Math.min(right1, right2);
-                    res = (n1 + n2) / 2.0;
+                if(totalLen % 2 == 0){
+                    int resLeft = Math.max(left1, left2);
+                    int resRight = Math.min(right1, right2);
+                    res = (resLeft + resRight) / 2.0;
                     return res;
                 }else{
                     return Math.max(left1, left2);
                 }
             }else{
                 if(left1 > right2){
-                    right = i - 1;
+                    right = mid1 - 1;
                 }else{
-                    left = i + 1;
+                    left = mid1 + 1;
                 }
             }
         }
