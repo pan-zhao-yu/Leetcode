a8775cccbcf64ed7468f0b70327c64461fbc92ec
Joey <59043772+pan-zhao-yu@users.noreply.github.com>
2025-07-21 17:39:38 +1000

    Time: 3 ms (100%), Space: 44.5 MB (97.21%) - LeetHub

--- 0155-min-stack/0155-min-stack.java
+++ 0155-min-stack/0155-min-stack.java
@@ -1,39 +1,50 @@
 class MinStack {
-    
-    private Node head;
-    public MinStack() {
-        
-    }
-    
-    public void push(int val) {
-        if(head == null){
-            head = new Node(val, val, null);
-        }else{
-            head = new Node(val, Math.min(val, head.min), head);
-        }
-    }
-    
-    public void pop() {
-        head = head.next;
-    }
-    
-    public int top() {
-        return head.value;
-    }
-    
-    public int getMin() {
-        return head.min;
-    }
-    
-    private class Node{
-        int value;
+    class Node{
+        int val;
         int min;
         Node next;
-        private Node(int value, int min, Node next){
-            this.value = value;
+        public Node(){}
+        public Node(int val){
+            this.val = val;
+        }
+        public Node(int val, int min, Node next){
+            this.val = val;
             this.min = min;
             this.next = next;
         }
+    }
+    Node head;
+    public MinStack() {
+        head = new Node();
+    }
+    
+    public void push(int val) {
+        Node curr = new Node(val);
+        curr.min = head.next == null ? val : Math.min(val, head.next.min);
+        curr.next = head.next;
+        head.next = curr;
+    }
+    
+    public void pop() {
+        if(head.next != null){
+            Node curr = head.next;
+            head.next = curr.next;
+            curr.next = null;
+        }
+    }
+    
+    public int top() {
+        if(head.next != null){
+            return head.next.val;
+        }
+        return -404;
+    }
+    
+    public int getMin() {
+        if(head.next != null){
+            return head.next.min;
+        }
+        return -404;
     }
 }
 
