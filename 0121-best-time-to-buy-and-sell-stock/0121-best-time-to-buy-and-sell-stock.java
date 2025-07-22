26edb4b094ace9323bd86d62d698871afcf362c1
Joey <59043772+pan-zhao-yu@users.noreply.github.com>
2025-07-23 00:24:50 +1000

    Time: 25 ms (1.93%), Space: 58.2 MB (97.82%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,26 +1,24 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int left = Integer.MAX_VALUE;
-        int profit = 0;
-        for(int i = 0; i < prices.length; i++){
-            left = Math.min(left, prices[i]);
-            profit = Math.max(profit, prices[i] - left);
+        int[][] dp = new int[prices.length][2];
+        dp[0][0] = 0;
+        dp[0][1] = -prices[0];
+        for(int i = 1; i < prices.length; i++){
+            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
+            dp[i][1] = Math.max(dp[i - 1][1], -prices[i]);
         }
-        return profit;
+        return dp[prices.length - 1][0];
     }
 }
 
 // class Solution {
 //     public int maxProfit(int[] prices) {
-//         int[][] dp = new int[prices.length][2];
-//         dp[0][0] = 0;
-//         dp[0][1] = -prices[0];
-//         for(int i = 1; i < prices.length; i++){
-//             dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
-//             dp[i][1] = Math.max(dp[i - 1][1], -prices[i]);
+//         int currLow = Integer.MAX_VALUE;
+//         int res = 0;
+//         for(int n : prices){
+//             currLow = Math.min(n, currLow);
+//             res = Math.max(res, n - currLow);
 //         }
-//         return dp[prices.length - 1][0];
+//         return res;
 //     }
 // }
-// //dp[i][0] means the max cash we can have if sell or not holding any stock on ith day
-// //dp[i][1] means the max cash we can have if buy or keep holding stock on ith day;
