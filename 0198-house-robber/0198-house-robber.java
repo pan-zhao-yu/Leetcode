c14cd275097bf1dccb67bead1e4ff8cb7f3c657f
Joey <59043772+pan-zhao-yu@users.noreply.github.com>
2025-07-24 03:13:36 +1000

    Time: 0 ms (100%), Space: 41.1 MB (46.05%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,13 +1,12 @@
 class Solution {
     public int rob(int[] nums) {
-        if(nums.length == 1) return nums[0];
-        if(nums.length == 2) return Math.max(nums[1], nums[0]);
-        int[] dp = new int[nums.length];
-        dp[0] = nums[0];
-        dp[1] = Math.max(nums[1], nums[0]);
-        for(int i = 2; i < nums.length; i++){
-            dp[i] = Math.max(dp[i - 2] + nums[i], dp[i - 1]);
+        int[][] dp = new int[nums.length][2];
+        dp[0][0] = 0;
+        dp[0][1] = nums[0];
+        for(int i = 1; i < nums.length; i++){
+            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1]);
+            dp[i][1] = Math.max(dp[i - 1][0] + nums[i], 0);
         }
-        return dp[nums.length - 1];
+        return Math.max(dp[nums.length - 1][0], dp[nums.length - 1][1]);
     }
 }
