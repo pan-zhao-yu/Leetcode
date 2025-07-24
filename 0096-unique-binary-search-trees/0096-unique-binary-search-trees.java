4ba985b7b0d31376ffe7275f23dc33fd54d391ea
Joey <59043772+pan-zhao-yu@users.noreply.github.com>
2025-07-25 04:22:35 +1000

    Time: 0 ms (100%), Space: 40.3 MB (78.95%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.java
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.java
@@ -2,8 +2,7 @@ class Solution {
     public int numTrees(int n) {
         int[] dp = new int[n + 1];
         dp[0] = 1;
-        dp[1] = 1;
-        for(int i = 2; i <= n; i++){
+        for(int i = 1; i <= n; i++){
             for(int j = 1; j <= i; j++){
                 dp[i] += dp[j - 1] * dp[i - j];
             }
@@ -11,5 +10,3 @@ class Solution {
         return dp[n];
     }
 }
-//dp[i] means how many unique ways that the tree head is from 1 to i
-//dp[i] += dp[i - 1] * dp[i - j];
