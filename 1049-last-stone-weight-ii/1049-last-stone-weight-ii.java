51b8c8ba8df0a6b94dada4f0f05d670eb459746d
Joey <59043772+pan-zhao-yu@users.noreply.github.com>
2025-07-26 00:10:23 +1000

    Time: 5 ms (31.89%), Space: 42.2 MB (34.06%) - LeetHub

--- 1049-last-stone-weight-ii/1049-last-stone-weight-ii.java
+++ 1049-last-stone-weight-ii/1049-last-stone-weight-ii.java
@@ -1,21 +1,22 @@
 class Solution {
     public int lastStoneWeightII(int[] stones) {
-        int total = 0;
-        for (int n : stones) {
-            total += n;
+        int sum = 0;
+        for(int n : stones){
+            sum += n;
         }
-        int target = total / 2;
-        int[][] dp = new int[stones.length + 1][target + 1];
-        
-        for (int i = 1; i < stones.length + 1; i++) {
-            for (int j = 0; j < target + 1; j++) {
-                if (stones[i - 1] <= j) {
-                    dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - stones[i - 1]] + stones[i - 1]);
-                } else {
-                    dp[i][j] = dp[i - 1][j];
-                }
+        int[][] dp = new int[sum / 2 + 1][stones.length];
+        for(int j = 0; j < stones.length; j++){
+            dp[0][j] = 0;
+        }
+        for(int i = 0; i < sum / 2 + 1; i++){
+            if(i >= stones[0]) dp[i][0] = stones[0];
+        }
+        for(int i = 1; i < sum / 2 + 1; i++){
+            for(int j = 1; j < stones.length; j++){
+                if(i >= stones[j]) dp[i][j] = Math.max(dp[i][j - 1], dp[i - stones[j]][j - 1] + stones[j]);
+                else dp[i][j] = dp[i][j - 1];
             }
         }
-        return total - dp[stones.length][target] * 2;
+        return sum - 2 * dp[sum / 2][stones.length - 1];
     }
 }
