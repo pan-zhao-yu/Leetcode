58a5d1f2daa73b6d09d9e23a83ad66e6df3172e6
Joey <59043772+pan-zhao-yu@users.noreply.github.com>
2025-07-26 17:10:03 +1000

    Time: 1 ms (43.67%), Space: 40.4 MB (68.72%) - LeetHub

--- 0343-integer-break/0343-integer-break.java
+++ 0343-integer-break/0343-integer-break.java
@@ -4,13 +4,11 @@ class Solution {
         dp[0] = 0;
         dp[1] = 1;
         dp[2] = 1;
-        for(int i = 3; i <= n; i++){
-            for(int j = 1; j < i; j++){
+        for(int i = 3; i < n + 1; i++){
+            for(int j = 0; j < i; j++){
                 dp[i] = Math.max(dp[i], Math.max(dp[i - j] * j, j * (i - j)));
             }
         }
         return dp[n];
     }
 }
-//dp[i] = j * (i - j);
-//dp[i] = dp[i - j] * j;
