5b56d7ab18bfd5c4c64674ca10f5f98e96b8f2f7
Joey <59043772+pan-zhao-yu@users.noreply.github.com>
2025-08-13 00:56:58 +1000

    Time: 6 ms (73.05%), Space: 44.9 MB (94.59%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,25 +1,26 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
         List<List<Integer>> graph = new ArrayList<>();
+        int[] indegree = new int[numCourses];
         for(int i = 0; i < numCourses; i++){
             graph.add(new ArrayList<>());
         }
-        int[] indegree = new int[numCourses];
         for(int[] pre : prerequisites){
             graph.get(pre[1]).add(pre[0]);
             indegree[pre[0]]++;
         }
         Queue<Integer> q = new ArrayDeque<>();
         for(int i = 0; i < numCourses; i++){
-            if(indegree[i] == 0) q.add(i);
+            if(indegree[i] == 0) q.offer(i);
         }
         int count = 0;
         while(!q.isEmpty()){
-            int c = q.poll();
+            int curr = q.poll();
             count++;
-            for(int pre : graph.get(c)){
-                indegree[pre]--;
-                if(indegree[pre] == 0) q.add(pre);
+            List<Integer> courseToTake= graph.get(curr);
+            for(int n : courseToTake){
+                indegree[n]--;
+                if(indegree[n] == 0) q.offer(n);
             }
         }
         return count == numCourses;
