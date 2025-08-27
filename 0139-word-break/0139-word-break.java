b79fc4f37c61de6cf9eb2f78d2ff543300b36c82
Joey <59043772+pan-zhao-yu@users.noreply.github.com>
2025-08-27 17:02:02 +1000

    Time: 9 ms (19.11%), Space: 45.2 MB (5.49%) - LeetHub

--- 0139-word-break/0139-word-break.java
+++ 0139-word-break/0139-word-break.java
@@ -1,15 +1,16 @@
 class Solution {
     public boolean wordBreak(String s, List<String> wordDict) {
         Set<String> set = new HashSet<>(wordDict);
-        boolean[] valid = new boolean[s.length() + 1];
-        valid[0] = true;
-        for(int i = 1; i <= s.length(); i++){
+        boolean[] dp = new boolean[s.length() + 1];
+        dp[0] = true;
+        for(int i = 1; i < s.length() + 1; i++){
             for(int j = 0; j < i; j++){
-                if(set.contains(s.substring(j, i)) && valid[j]){
-                    valid[i] = true;
+                String sub = s.substring(j, i);
+                if(dp[j] && set.contains(sub)){
+                    dp[i] = true;
                 }
             }
         }
-        return valid[s.length()];
+        return dp[s.length()];
     }
 }
