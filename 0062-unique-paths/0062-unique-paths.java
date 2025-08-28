40149cf1bed0aa4ed5f68222abf6fc8f08fbc261
Joey <59043772+pan-zhao-yu@users.noreply.github.com>
2025-08-29 00:19:18 +1000

    Time: 0 ms (100%), Space: 40.9 MB (16.72%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,17 +1,18 @@
 class Solution {
     public int uniquePaths(int m, int n) {
         int[][] dp = new int[m][n];
-        for(int r = 0; r < m; r++){
-            dp[r][0] = 1;
+        for(int i = 0; i < m; i++){
+            dp[i][0] = 1;
         }
-        for(int c = 0; c < n; c++){
-            dp[0][c] = 1;
+        for(int j = 0; j < n; j++){
+            dp[0][j] = 1;
         }
-        for(int r = 1; r < m; r++){
-            for(int c = 1; c < n; c++){
-                dp[r][c] = dp[r - 1][c] + dp[r][c - 1];
+        for(int i = 1; i < m; i++){
+            for(int j = 1; j < n; j++){
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
             }
         }
         return dp[m - 1][n - 1];
     }
 }
+//dp[i][j] means, unique path to get to dp[i][j]; (from top + from left)
